f7c50fb2ae6737ad9a36ce92bddb33f1851856c7
Peter Withers <peter.withers@mpi.nl>
2018-04-06 14:12:14 +0200

    Added maven output to the build results.

--- deploy-by-hook.js
+++ deploy-by-hook.js
@@ -147,7 +147,7 @@ function deployStagingGui(listing, currentEntry) {
 //                    'experiment.staticFilesUrl': stagingServerUrl
     }).then(function (value) {
         console.log("frinex-gui finished");
-        storeResult(currentEntry.buildName, "<a href=\'" + currentEntry.buildName + "staging.html\'>deployed</a><a href=\'" + currentEntry.buildName + "staging.war\'>war</a><a href=\'ems13/" + currentEntry.buildName + "\'>ems13</a>", "staging", "web", false, false);
+        storeResult(currentEntry.buildName, '<a href="' + currentEntry.buildName + 'staging.war">war</a><a href="ems13/' + currentEntry.buildName + '">ems13</a>', "staging", "web", false, false);
 //        var successFile = fs.createWriteStream(targetDirectory + "/" + currentEntry.buildName + "staging.html", {flags: 'w'});
 //        successFile.write(value);
 //        console.log(value);
@@ -160,7 +160,7 @@ function deployStagingGui(listing, currentEntry) {
         console.log(reason);
         console.log("frinex-gui staging failed");
         console.log(currentEntry.experimentDisplayName);
-        storeResult(currentEntry.buildName, "<a href='" + currentEntry.experimentDisplayName + "staging.html'>failed</a>", "staging", "web", true, false);
+        storeResult(currentEntry.buildName, '<a href="' + currentEntry.experimentDisplayName + 'staging.html">failed</a>', "staging", "web", true, false);
         var errorFile = fs.createWriteStream(targetDirectory + "/" + currentEntry.experimentDisplayName + "staging.html", {flags: 'w'});
         errorFile.write(reason);
         buildNextExperiment(listing);
