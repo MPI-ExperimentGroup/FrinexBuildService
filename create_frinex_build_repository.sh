cf1808b68ac4a361390bdab9711c16382fec8d82
Peter Withers <peter.withers@mpi.nl>
2018-09-14 13:18:14 +0200

    Updated the script which creates separated GIT repositories for use in the Frinex build process.

--- create_frinex_build_repository.sh
+++ create_frinex_build_repository.sh
@@ -29,36 +29,43 @@ if [[ $# -eq 0 ]] ; then
     echo 'please provide the target repository name as the first argument'
     exit 0
 fi
+echo /srv/git/$1.git
+if [ -d /srv/git/$1.git ];
+then
+    echo "target git repository already exists";
+    exit 0
+fi
 
-if [ -f /srv/git/$1.git ];
+if [ -d /srv/$1 ];
 then
     echo "target already exists";
     exit 0
-else
-    continue
 fi
 
 # initialise the repository
-git init --bare /path/to/bare/$1.git
+git init --bare /srv/git/$1.git
 
 # add the repository to the list of conflict check locations
-sed -i 's/listingJsonFiles = /listingJsonFiles = /srv/$1/listing.json,/g' /srv/ExperimentTemplate/publish.properties
+sed -i "s/listingJsonFiles\ =\ /listingJsonFiles\ =\ \/srv\/$1\/listing\.json\,/g" /srv/ExperimentTemplate/publish.properties
 
 # add the post-receive hook
-cp /srv/ExperimentTemplate/post-receive /srv/git/$1.git/hooks/post-receive
+#cp /srv/ExperimentTemplate/post-receive /srv/git/$1.git/hooks/post-receive
+cp /srv/git/maarten.git/hooks/post-receive /srv/git/$1.git/hooks/post-receive
+sed -i "s/maarten/$1/g" /srv/git/$1.git/hooks/post-receive
+diff /srv/git/maarten.git/hooks/post-receive /srv/git/$1.git/hooks/post-receive
 
 # set the permissions
-chown -R wwwrun /srv/git/$1.git
 chmod -R g+rwx /srv/git/$1.git
 chmod -R u+rwx /srv/git/$1.git
+chown -R wwwrun /srv/git/$1.git
 
 # add the git user
-htpasswd /srv/git/.htpasswd $1
+#htpasswd /srv/git/.htpasswd $1
 
 # check out the repository for use in the build process
-cd /srv; git clone git/$1.git
+#cd /srv; git clone git/$1.git
 
 # set the permissions
-chown -R wwwrun /srv/$1
 chmod -R g+rwx /srv/$1
 chmod -R u+rwx /srv/$1
+chown -R wwwrun /srv/$1
