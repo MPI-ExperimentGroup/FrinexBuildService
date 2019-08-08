1b1d411e0a87183e899d4049f010566b355eebac
Peter Withers <peter.withers@mpi.nl>
2019-08-08 14:10:49 +0200

    Updated the script used to create experiment repositories and updated the post commit hook.

--- create_frinex_build_repository.sh
+++ create_frinex_build_repository.sh
@@ -24,40 +24,50 @@
 
 # This script creates a separated repository for use in the Frinex build process
 # when commits are pushed to the resulting GIT repository any JSON and XML experiment configuration files will be built according to the rules in listing.json
+cd $(dirname "$0")
+scriptDir=$(pwd -P)
+targetDir=$(dirname "$scriptDir")/frinex-repositories
+echo "scriptDir: $scriptDir"
+echo "targetDir: $targetDir"
+
+mkdir $(dirname "$scriptDir")/target
 
 if [[ $# -eq 0 ]] ; then
     echo 'please provide the target repository name as the first argument'
     exit 0
 fi
-echo /srv/git/$1.git
-if [ -d /srv/git/$1.git ];
+
+echo $targetDir/git/$1.git
+if [ -d $targetDir/git/$1.git ];
 then
     echo "target git repository already exists";
     exit 0
 fi
 
-if [ -d /srv/$1 ];
+echo $targetDir/$1
+if [ -d $targetDir/$1 ];
 then
     echo "target already exists";
     exit 0
 fi
 
 # initialise the repository
-git init --bare /srv/git/$1.git
+git init --bare $targetDir/git/$1.git
 
 # add the repository to the list of conflict check locations
-sed -i "s/listingJsonFiles\ =\ /listingJsonFiles\ =\ \/srv\/$1\/listing\.json\,/g" /srv/ExperimentTemplate/publish.properties
+# todo: un-macify this
+sed -i'.tmp' -e "s/listingJsonFiles\ =\ /listingJsonFiles\ =\ \.\.\/$1\/listing\.json\,/g" $scriptDir/publish.properties
 
 # add the post-receive hook
 #cp /srv/ExperimentTemplate/post-receive /srv/git/$1.git/hooks/post-receive
-cp /srv/git/maarten.git/hooks/post-receive /srv/git/$1.git/hooks/post-receive
-sed -i "s/maarten/$1/g" /srv/git/$1.git/hooks/post-receive
-diff /srv/git/maarten.git/hooks/post-receive /srv/git/$1.git/hooks/post-receive
+sed "s/RepositoryName/$1/g" $scriptDir/post-receive > $targetDir/git/$1.git/hooks/post-receive
+#sed -i "s/maarten/$1/g" /srv/git/$1.git/hooks/post-receive
+#diff /srv/git/maarten.git/hooks/post-receive /srv/git/$1.git/hooks/post-receive
 
 # set the permissions
-chmod -R g+rwx /srv/git/$1.git
-chmod -R u+rwx /srv/git/$1.git
-chown -R wwwrun /srv/git/$1.git
+chmod -R g+rwx $targetDir/git/$1.git
+chmod -R u+rwx $targetDir/git/$1.git
+#chown -R wwwrun /srv/git/$1.git
 
 # add the git user
 #htpasswd /srv/git/.htpasswd $1
@@ -66,6 +76,18 @@ chown -R wwwrun /srv/git/$1.git
 #cd /srv; git clone git/$1.git
 
 # set the permissions
-chmod -R g+rwx /srv/$1
-chmod -R u+rwx /srv/$1
-chown -R wwwrun /srv/$1
+#chmod -R g+rwx /srv/$1
+#chmod -R u+rwx /srv/$1
+#chown -R wwwrun /srv/$1
+
+
+
+# todo: remove the following once testing is complete
+cd $targetDir
+git clone git/$1.git
+cd $1
+cp ~/Documents/GitHub/ladd/rosselfieldkit1.xml .
+git add rosselfieldkit1.xml 
+git commit -m "test" rosselfieldkit1.xml; git push
+echo " " >> rosselfieldkit1.xml
+git commit -m "test" rosselfieldkit1.xml; git push
