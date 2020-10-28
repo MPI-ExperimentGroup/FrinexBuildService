ef56ddb06f2d22b67fa26e3ab7e20f7e4a96092b
Peter Withers <peter-gthb@bambooradical.com>
2020-10-28 12:08:13 +0100

    Set the docker image name to that used in the Frinex build process.

--- start_frinexbuild_container.sh
+++ start_frinexbuild_container.sh
@@ -26,10 +26,10 @@
 git pull
 
 # build the frinexbuild dockerfile
-#docker build --no-cache -f frinexbuild.Dockerfile -t frinexbuild .
+docker build --no-cache -f frinexbuild.Dockerfile -t frinexbuild .
 
-# build the frinexcompiler dockerfile:
-#docker build --rm -f frinexcompiler.Dockerfile -t frinexcompiler:latest .
+# build the frinexapps dockerfile:
+docker build --rm -f frinexapps.Dockerfile -t frinexapps:latest .
 
-# start the frinexbuild container with access to docker.sock so that it can create sibling containers of frinexcompiler
+# start the frinexbuild container with access to docker.sock so that it can create sibling containers of frinexapps
 docker run  -v /var/run/docker.sock:/var/run/docker.sock --rm -it --name frinexbuild-test01 -p 8080:80 frinexbuild sh
