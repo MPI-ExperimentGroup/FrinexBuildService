8d45d1405e8814168a4cf8d7ba7c0203df22eb5a
Peter Withers <peter-gthb@bambooradical.com>
2021-01-15 12:35:39 +0100

    Updated the local port to the container.

--- test_repository_push.sh
+++ test_repository_push.sh
@@ -2,7 +2,7 @@
 rm -rf test_co_directory
 mkdir test_co_directory
 cd test_co_directory
-git clone http://localhost:8081/git/TEST_REPOSITORY.git/
+git clone http://localhost:8070/git/TEST_REPOSITORY.git/
 if [ -d TEST_REPOSITORY ]; then 
     cd TEST_REPOSITORY
     # do a commit and push
