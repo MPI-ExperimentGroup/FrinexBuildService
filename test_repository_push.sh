6f5d33782aca4ac7bfc9958807c5d3f88d6328d1
Peter Withers <peter-gthb@bambooradical.com>
2021-02-23 09:44:12 +0100

    Moved testing scripts.

--- test_repository_push.sh
+++ test_repository_push.sh
@@ -6,16 +6,16 @@ git clone http://localhost:9070/git/TEST_REPOSITORY.git/
 if [ -d TEST_REPOSITORY ]; then 
     cd TEST_REPOSITORY
     # do a commit and push
-    cp ~/Documents/Frinex/experiments/with_stimulus_example.xml .
-    git add *.xml; git commit -m "Adding XML files" *.xml; git push
-    echo "bla" >> test_data.txt; git commit -m "modified text file" test_data.txt; git pull; git push --verbose
-    cp -r ~/Documents/Frinex/experiments/with_stimulus_example .
-    git add with_stimulus_example; git commit -m "Adding with_stimulus_example files" with_stimulus_example; git push
-    sed -i ".tmp" "s|state=\"production\"|state=\"undeploy\"|g" with_stimulus_example.xml
-    git add *.xml; git commit -m "Adding XML files" *.xml; git push
-    cp ~/Documents/Frinex/experiments/with_stimulus_example.xml .
+    #cp ~/Documents/Frinex/experiments/with_stimulus_example.xml .
+    #git add *.xml; git commit -m "Adding XML files" *.xml; git push
+    #echo "bla" >> test_data.txt; git commit -m "modified text file" test_data.txt; git pull; git push --verbose
+    #cp -r ~/Documents/Frinex/experiments/with_stimulus_example .
+    #git add with_stimulus_example; git commit -m "Adding with_stimulus_example files" with_stimulus_example; git push
+    #sed -i ".tmp" "s|state=\"production\"|state=\"undeploy\"|g" with_stimulus_example.xml
+    #git add *.xml; git commit -m "Adding XML files" *.xml; git push
+    #cp ~/Documents/Frinex/experiments/with_stimulus_example.xml .
     #echo " " >> with_stimulus_example.xml
-    git add *.xml; git commit -m "Adding XML files" *.xml; git push
+    #git add *.xml; git commit -m "Adding XML files" *.xml; git push
     #cp ~/Documents/Frinex/experiments/*.json .
     #git add *.json; git commit -m "Adding JSON files" *.json; git push
     #cp ~/Documents/Frinex/*/s*.xml .
@@ -23,7 +23,6 @@ if [ -d TEST_REPOSITORY ]; then
     #cp ~/Documents/Frinex/*/*.xml .
     #git add *.xml; git commit -m "Adding XML files" *.xml; git push
     #sed -i ".tmp" "s|textFontSize=\"17\">|textFontSize=\"17\"><deployment publishDate=\"2020-02-02\" expiryDate=\"2025-02-22\" isWebApp=\"true\" isDesktop=\"true\" isiOS=\"true\" isAndroid=\"true\" state=\"staging\" />|g" *.xml
-    #sed -i ".tmp" "s|<experiment\([^>]*\)>|<experiment\1><deployment state=\"production\" />|g" sentencemonitoring_bq4_timestudy.xml
     #git add *.xml; git commit -m "Adding XML files" *.xml; git push
     #sed -i ".tmp" "s|<validationService>|<deployment publishDate=\"2020-02-02\" expiryDate=\"2025-02-22\" isWebApp=\"true\" isDesktop=\"true\" isiOS=\"true\" isAndroid=\"true\" state=\"staging\" /><validationService>|g" *.xml
     #git add *.xml; git commit -m "Adding XML files" *.xml; git push
