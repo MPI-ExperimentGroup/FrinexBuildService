42603f60a3e45ec3c8d4abe283bf4616da2152e9
Peter Withers <peter-gthb@bambooradical.com>
2021-05-20 14:20:39 +0200

    Started adding a frinex_db_manager container to handle the creation of databases are quired by new experiments, which replaces the existing cron job that until now has done this task.

--- install_frinexbuild_container.sh
+++ install_frinexbuild_container.sh
@@ -62,7 +62,7 @@ else
     docker container rm frinex_db_manager 
 
     # start the frinex_db_manager in the bridge network
-    docker run  --restart unless-stopped --net frinex_db_manager_net --name frinex_db_manager -d --rm frinex_db_manager:latest
+    docker run --restart unless-stopped --net frinex_db_manager_net --name frinex_db_manager -d frinex_db_manager:latest
 
     # remove the old frinexbuild
     docker stop frinexbuild 
