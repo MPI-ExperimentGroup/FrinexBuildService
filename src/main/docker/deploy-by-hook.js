18d042761c1acef6ff24750f2eea2439802e547e
Peter Withers <peter-gthb@bambooradical.com>
2021-07-20 13:51:34 +0200

    Added links to the stable, beta and latest versions of the documentation and schema files.

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -79,10 +79,10 @@ function startResult() {
     fs.writeSync(resultsFile, "<br/><br/>\n");
     fs.writeSync(resultsFile, "<a href='frinex.html'>Current XML Documentation</a>&nbsp;\n");
     fs.writeSync(resultsFile, "<a href='frinex.xsd'>Current XML Schema</a><br/>\n");
-    fs.writeSync(resultsFile, "To build with the current stable either omit the frinexVersion attribute or specify frinexVersion=\"stable\" and set the schema location in your XML to use \"frinex.xsd\".<br/>\n");
+    fs.writeSync(resultsFile, "To build with the current stable either omit the frinexVersion attribute or specify frinexVersion=\"stable\" and set the schema location in your XML to use \"frinex.xsd\".<br/><br/>\n");
     fs.writeSync(resultsFile, "<a href='beta.html'>Release candidate XML Documentation</a>&nbsp;\n");
     fs.writeSync(resultsFile, "<a href='beta.xsd'>Release candidate XML Schema</a><br/>\n");
-    fs.writeSync(resultsFile, "To build with the release candidate frinexVersion=\"beta\" and set the schema location in your XML to use \"beta.xsd\".<br/>\n");
+    fs.writeSync(resultsFile, "To build with the release candidate frinexVersion=\"beta\" and set the schema location in your XML to use \"beta.xsd\".<br/><br/>\n");
     fs.writeSync(resultsFile, "<a href='beta.html'>Latest snapshot XML Documentation</a>&nbsp;\n");
     fs.writeSync(resultsFile, "<a href='beta.xsd'>Latest snapshot XML Schema</a><br/>\n");
     fs.writeSync(resultsFile, "The latest snapshot changes very frequently and is not usually recommended but can be used with frinexVersion=\"latest\" and the schema location in your XML using \"latest.xsd\".<br/>\n");
