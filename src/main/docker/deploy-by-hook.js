d240a8da9771c6588af62d792e053f84a6a5faa1
Peter Withers <peter.withers@mpi.nl>
2021-09-14 16:41:48 +0200

    Updating the server status indicator to be refreshed more often.

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -120,11 +120,11 @@ function startResult() {
     fs.writeSync(resultsFile, "<a href='git-push-err.txt'>err</a>&nbsp;\n");
     fs.writeSync(resultsFile, "<script>\n");
     fs.writeSync(resultsFile, "var applicationStatus = {};\n");
-    fs.writeSync(resultsFile, "function updateDeploymentStatus(keyString, cellString) {\n");
+    fs.writeSync(resultsFile, "function updateDeploymentStatus(keyString, cellString, cellStyle) {\n");
     fs.writeSync(resultsFile, "var experimentCell = document.getElementById(keyString + '_' + cellString);\n");
     fs.writeSync(resultsFile, "if (experimentCell) {\n");
     fs.writeSync(resultsFile, "var statusStyle = (keyString + '_' + cellString in applicationStatus)?';border-right: 3px solid ' + applicationStatus[keyString + '_' + cellString] + ';':'';\n");
-    fs.writeSync(resultsFile, "experimentCell.style = data.table[keyString][cellString].style + statusStyle;\n");
+    fs.writeSync(resultsFile, "experimentCell.style = cellStyle + statusStyle;\n");
     fs.writeSync(resultsFile, "}\n");
     fs.writeSync(resultsFile, "}\n");
     fs.writeSync(resultsFile, "function doUpdate() {\n");
@@ -154,7 +154,7 @@ function startResult() {
     fs.writeSync(resultsFile, "} else {\n");
     fs.writeSync(resultsFile, "applicationStatus[experimentName + '__staging_admin'] = 'red';\n");
     fs.writeSync(resultsFile, "}\n");
-    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString);\n");
+    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString, data.table[keyString][cellString].style);\n");
     fs.writeSync(resultsFile, "}\n");
     fs.writeSync(resultsFile, "});\n");
     fs.writeSync(resultsFile, "};}(keyString)));\n");
@@ -167,7 +167,7 @@ function startResult() {
     fs.writeSync(resultsFile, "} else {\n");
     fs.writeSync(resultsFile, "applicationStatus[experimentName + '__production_admin'] = 'red';\n");
     fs.writeSync(resultsFile, "}\n");
-    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString);\n");
+    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString, data.table[keyString][cellString].style);\n");
     fs.writeSync(resultsFile, "}\n");
     fs.writeSync(resultsFile, "});\n");
     fs.writeSync(resultsFile, "};}(keyString)));\n");
@@ -180,7 +180,7 @@ function startResult() {
     fs.writeSync(resultsFile, "} else {\n");
     fs.writeSync(resultsFile, "applicationStatus[experimentName + '__staging_admin'] = 'red';\n");
     fs.writeSync(resultsFile, "}\n");
-    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString);\n");
+    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString, data.table[keyString][cellString].style);\n");
     fs.writeSync(resultsFile, "}\n");
     fs.writeSync(resultsFile, "});\n");
     fs.writeSync(resultsFile, "};}(keyString)));\n");
@@ -192,7 +192,7 @@ function startResult() {
     fs.writeSync(resultsFile, "} else {\n");
     fs.writeSync(resultsFile, "applicationStatus[experimentName + '__production_admin'] = 'red';\n");
     fs.writeSync(resultsFile, "}\n");
-    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString);\n");
+    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString, data.table[keyString][cellString].style);\n");
     fs.writeSync(resultsFile, "}\n");
     fs.writeSync(resultsFile, "});\n");
     fs.writeSync(resultsFile, "};}(keyString)));\n");
@@ -215,7 +215,7 @@ function startResult() {
     fs.writeSync(resultsFile, "document.getElementById(keyString + '_' + cellString).innerHTML = data.table[keyString][cellString].value + buildTimeSting;\n");
     fs.writeSync(resultsFile, "}\n");
     //fs.writeSync(resultsFile, "var statusStyle = ($.inArray(keyString + '_' + cellString, applicationStatus ) >= 0)?';border-right: 5px solid green;':';border-right: 5px solid grey;';\n");
-    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString);\n");
+    fs.writeSync(resultsFile, "updateDeploymentStatus(keyString, cellString, data.table[keyString][cellString].style);\n");
     fs.writeSync(resultsFile, "}\n");
     fs.writeSync(resultsFile, "}\n");
     fs.writeSync(resultsFile, "}\n");
