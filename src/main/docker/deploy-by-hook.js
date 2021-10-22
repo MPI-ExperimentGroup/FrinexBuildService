221636b808ab13be0b0b0c623d6440ea4f2214da
Peter Withers <peter.withers@mpi.nl>
2021-10-22 13:41:26 +0200

    Added the experiment name as the URL path when starting as Docker services.

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -526,8 +526,7 @@ function deployDockerService(currentEntry, warFileName, serviceName) {
     fs.writeFileSync(dockerFilePath,
         "FROM openjdk:11\n"
         + "COPY " + warFileName + " /" + warFileName + "\n"
-        + "CMD [\"java\", \"-jar\", \"/" + warFileName + "\"]\n"
-        // TODO: it should not be necessary to service stop first, but this needs to be tested 
+        + "CMD [\"java\", \"-jar\", \"/" + warFileName + "\", \"--server.servlet.context-path=/" + serviceName + "\"]\n"
         // TODO: it should not be necessary to do a service start, but this needs to be tested 
         // note that manually stopping the services will cause an outage whereas replacing the service will minimise service disruption
         , { mode: 0o755 });
