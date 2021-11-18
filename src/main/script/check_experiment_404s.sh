42a71962baad100973ed8f1e8a5bf7db28a64d6d
Peter Withers <peter.withers@mpi.nl>
2021-11-18 15:16:03 +0100

    Added support for 404s on the experiment name only without a trailing / or path in the URL.

--- src/main/script/check_experiment_404s.sh
+++ src/main/script/check_experiment_404s.sh
@@ -46,7 +46,7 @@ cat $lockFile >> $scriptDir/check_experiment_404s_$(date +%F).log
 date >> $scriptDir/check_experiment_404s_$(date +%F).log
 # grep any 404 lines, and extract the first element in the URL path that coresponds with the experiment name, then sanity check with grep again to filter out lines containing non alpha numeric characters, then remove duplicate names
 # requests to /actuator/health are ignored because this would be the build page not an active user
-possibleExperiment404sWithAdmin=$(sudo grep -E "GET /[a-z][a-z0-9_]{3,}(-admin)?/.*/.* 404" /var/log/tomcat/localhost_access_log.$(date +%F).txt | grep -v "/health HTTP" | cut -d / -f 4 | grep -E "^[a-z][a-z0-9_]{3,}(-admin)?$" | sort | uniq | paste -sd "|")
+possibleExperiment404sWithAdmin=$(sudo grep -E "GET /[a-z][a-z0-9_]{3,}(-admin)?/?.* .*/.* 404" /var/log/tomcat/localhost_access_log.$(date +%F).txt | grep -v "/health HTTP" | cut -d " " -f 7 | cut -d / -f 2 | grep -E "^[a-z][a-z0-9_]{3,}(-admin)?$" | sort | uniq | paste -sd "|")
 possibleExperiment404s=$(echo "|$possibleExperiment404sWithAdmin|" | sed "s/-admin|/|/g")
 #echo $possibleExperiment404s
 
