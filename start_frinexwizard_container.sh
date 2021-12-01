62e4a21b5745c626502061ba3d273d3afd806161
Peter Withers <peter-gthb@bambooradical.com>
2021-12-01 14:21:08 +0100

    Added a docker image for the frinex wizard.

---
+++ start_frinexwizard_container.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright (C) 2021 Max Planck Institute for Psycholinguistics
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+#
+# @since 01 December 2021 12:50 PM (creation date)
+# @author Peter Withers <peter.withers@mpi.nl>
+#
+
+cd $(dirname "$0")
+workingDir=$(pwd -P)
+
+# check that the properties to be used match the current machine
+if ! grep -q $(hostname) src/main/config/publish.properties; then 
+    echo "Aborting because the publish.properties does not match the current machine.";
+else
+    # remove the old frinexwizard
+    docker stop frinexwizaed
+    docker container rm frinexwizard
+
+    # start the frinexwizard container
+    docker run --restart unless-stopped -dit --name frinexwizard  -p 7070:80 frinexwizard:latest
+fi;
