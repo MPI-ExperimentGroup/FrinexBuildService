d720f8aaa72050b86cde4b6eef6e06ec4d2a8484
Peter Withers <peter-gthb@bambooradical.com>
2022-01-10 14:08:02 +0100

    Removed the frinexapps build process frm the frinexbuild install because this can better be done with the build alpha/beta actions.

--- install_frinexbuild_container.sh
+++ install_frinexbuild_container.sh
@@ -37,7 +37,7 @@ else
     docker build --no-cache -f docker/frinexbuild.Dockerfile -t frinexbuild:latest .
 
     # build the frinexapps dockerfile:
-    docker build --rm -f docker/frinexapps.Dockerfile -t frinexapps:latest .
+    # docker build --rm -f docker/frinexapps.Dockerfile -t frinexapps:latest .
 
     # build a tomcat docker image to test deployments
     #docker build --rm -f docker/tomcatstaging.Dockerfile -t tomcatstaging:latest .
