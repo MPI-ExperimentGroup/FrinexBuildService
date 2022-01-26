e60e0bb03b127ca14fab9891aa76d183dc492335
Peter Withers <peter-gthb@bambooradical.com>
2022-01-26 16:26:17 +0100

    Updated the configuration filter with better LDAP defaults and checks for files for each build context that the script is used in.

--- src/main/docker/filter_config_files.sh
+++ src/main/docker/filter_config_files.sh
@@ -2,25 +2,31 @@
 
 # this script applies location specific settings in the various configuration files during the docker image build process.
 
+if [ -e /FrinexBuildService/frinex-git-server.conf ]; then
 # Frinex Build settings
-sed -i "s|UrlLDAP|example.com|g" /FrinexBuildService/frinex-git-server.conf 
-sed -i "s|DcLDAP|DC=example,DC=com|g" /FrinexBuildService/frinex-git-server.conf 
-sed -i "s|UserLDAP|example|g" /FrinexBuildService/frinex-git-server.conf 
-sed -i "s|PassLDAP|example|g" /FrinexBuildService/frinex-git-server.conf 
+sed -i "s|UrlLDAP|example.com|g" /FrinexBuildService/frinex-git-server.conf
+sed -i "s|DcLDAP|DC=example,DC=com|g" /FrinexBuildService/frinex-git-server.conf
+sed -i "s|UserLDAP|example|g" /FrinexBuildService/frinex-git-server.conf
+sed -i "s|PassLDAP|example|g" /FrinexBuildService/frinex-git-server.conf
 sed -i "s|#LDAPOPTION||g" /FrinexBuildService/frinex-git-server.conf 
 #sed -i "s|#PUBLICOPTION||g" /FrinexBuildService/frinex-git-server.conf 
+fi
 
+if [ -e /FrinexBuildService/cgi/repository_setup.cgi ]; then
 # Frinex Repository settings
 sed -i "s|BuildServerUrl|http://example.com|g" /FrinexBuildService/cgi/repository_setup.cgi
+fi
 
+if [ -e /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties ]; then
 # Frinex Wizard settings
-sed -i "s|ldaps://ldap.example.com:33389/dc=myco,dc=org|ldap://example.com:389/DC=example,DC=com|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
-sed -i "s|ou=exampleGroups|ou=example|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
-sed -i "s|exampleAttribute|example|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
-#sed -i "s|uid=admin,ou=system|uid=example|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
+sed -i "s|ldaps://ldap.example.com:33389/dc=myco,dc=org|ldaps://example.com:636/CN=Users,DC=example,DC=com|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
+sed -i "s|ou=exampleGroups|ou=groups|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
+sed -i "s|exampleAttribute|passwordAttribute|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
+#RUN sed -i "s|uid=admin,ou=system|uid=example|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
 sed -i "s|uid=admin,ou=system|example|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
 sed -i "s|managerDnPassword|example|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
 #sed -i "s|uid={0},ou=examplePattern|CN={0},CN=Users|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
 sed -i "s|uid={0},ou=examplePattern|userPrincipalName={0}|g" /ExperimentTemplate/ExperimentDesigner/src/main/resources/application.properties
 #sed -i "s|.anyRequest().authenticated()|.anyRequest().fullyAuthenticated()|g" /ExperimentTemplate/ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/WebSecurityConfig.java
 # this option disables authentication in the wizard: sed -i "s|.anyRequest().authenticated()|.anyRequest().permitAll()|g" /ExperimentTemplate/ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/WebSecurityConfig.java
+fi
