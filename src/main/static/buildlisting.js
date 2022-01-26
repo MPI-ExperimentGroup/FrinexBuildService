aa45c739a4e1b00b710315efcbe911e986d39720
Peter Withers <peter-gthb@bambooradical.com>
2022-01-26 15:36:04 +0100

    Extracted the build listing HTML and JS from the Node script into static files.

--- src/main/static/buildlisting.js
+++ src/main/static/buildlisting.js
@@ -33,7 +33,9 @@ function updateDeploymentStatus(keyString, cellString, cellStyle) {
 function doUpdate() {
     updateTimer = window.setTimeout(doUpdate, 60000);
     $.getJSON('buildhistory.json?' + new Date().getTime(), function (data) {
+        //console.log(data);
         for (var keyString in data.table) {
+            //console.log(keyString);
             var experimentRow = document.getElementById(keyString + '_row');
             if (!experimentRow) {
                 var tableRow = document.createElement('tr');
@@ -45,7 +47,10 @@ function doUpdate() {
                     tableRow.appendChild(tableCell);
                 }
                 document.getElementById('buildTable').appendChild(tableRow);
-                $.getJSON('https://frinexstaging.mpi.nl/' + keyString + '-admin/health', (function (experimentName, cellStyle) {
+                // check the spring health here and show http and db status via applicationStatus array
+                // getting the health of the experiment admin and web
+                // the path -admin/health is for spring boot 1.4.1
+                $.getJSON(data.stagingServerUrl + '/' + keyString + '-admin/health', (function (experimentName, cellStyle) {
                     return function (data) {
                         $.each(data, function (key, val) {
                             if (key === 'status') {
@@ -59,7 +64,8 @@ function doUpdate() {
                         });
                     };
                 }(keyString, data.table[keyString]['_staging_admin'].style)));
-                $.getJSON('https://frinexproduction.mpi.nl/' + keyString + '-admin/health', (function (experimentName, cellStyle) {
+                // this request is for spring boot 1.4.1 and Frinex only had a single production server at that time, so we only check the default server here
+                $.getJSON(data.productionServerUrl + '/' + keyString + '-admin/health', (function (experimentName, cellStyle) {
                     return function (data) {
                         $.each(data, function (key, val) {
                             if (key === 'status') {
@@ -73,7 +79,8 @@ function doUpdate() {
                         });
                     };
                 }(keyString, data.table[keyString]['_production_admin'].style)));
-                $.getJSON('https://frinexstaging.mpi.nl/' + keyString + '-admin/actuator/health', (function (experimentName, cellStyle) {
+                // the path -admin/actuator/health is for spring boot 2.3.0
+                $.getJSON(data.stagingServerUrl + '/' + keyString + '-admin/actuator/health', (function (experimentName, cellStyle) {
                     return function (data) {
                         $.each(data, function (key, val) {
                             if (key === 'status') {
@@ -87,7 +94,7 @@ function doUpdate() {
                         });
                     };
                 }(keyString, data.table[keyString]['_staging_admin'].style)));
-                $.getJSON(((typeof data.table[keyString]['_production_target'] !== 'undefined' && data.table[keyString]['_production_target'].value != '') ? data.table[keyString]['_production_target'].value : 'https://frinexproduction.mpi.nl') + '/' + keyString + '-admin/actuator/health', (function (experimentName, cellStyle) {
+                $.getJSON(((typeof data.table[keyString]['_production_target'] !== 'undefined' && data.table[keyString]['_production_target'].value != '') ? data.table[keyString]['_production_target'].value : data.productionServerUrl) + '/' + keyString + '-admin/actuator/health', (function (experimentName, cellStyle) {
                     return function (data) {
                         $.each(data, function (key, val) {
                             if (key === 'status') {
@@ -101,7 +108,8 @@ function doUpdate() {
                         });
                     };
                 }(keyString, data.table[keyString]['_production_admin'].style)));
-                $.getJSON('https://frinexstaging.mpi.nl/' + keyString + '/actuator/health', (function (experimentName, cellStyle) {
+                // get the health of the GUI
+                $.getJSON(data.stagingServerUrl + '/' + keyString + '/actuator/health', (function (experimentName, cellStyle) {
                     return function (data) {
                         $.each(data, function (key, val) {
                             if (key === 'status') {
@@ -115,7 +123,7 @@ function doUpdate() {
                         });
                     };
                 }(keyString, data.table[keyString]['_staging_web'].style)));
-                $.getJSON(((typeof data.table[keyString]['_production_target'] !== 'undefined' && data.table[keyString]['_production_target'].value != '') ? data.table[keyString]['_production_target'].value : 'https://frinexproduction.mpi.nl') + '/' + keyString + '/actuator/health', (function (experimentName, cellStyle) {
+                $.getJSON(((typeof data.table[keyString]['_production_target'] !== 'undefined' && data.table[keyString]['_production_target'].value != '') ? data.table[keyString]['_production_target'].value : data.productionServerUrl) + '/' + keyString + '/actuator/health', (function (experimentName, cellStyle) {
                     return function (data) {
                         $.each(data, function (key, val) {
                             if (key === 'status') {
@@ -130,9 +138,11 @@ function doUpdate() {
                     };
                 }(keyString, data.table[keyString]['_production_web'].style)));
             }
+            // use the UTC date stored in a data attribute of the row to check if the row has changes before updating it
             if (data.table[keyString]['_date'].value !== experimentRow.dataset.lastchange) {
                 experimentRow.dataset.lastchange = data.table[keyString]['_date'].value;
                 for (var cellString in data.table[keyString]) {
+                    //console.log(cellString);
                     var experimentCell = document.getElementById(keyString + '_' + cellString);
                     if (!experimentCell) {
                         var tableCell = document.createElement('td');
@@ -146,6 +156,7 @@ function doUpdate() {
                         var buildTimeSting = (typeof data.table[keyString][cellString].ms !== 'undefined' && data.table[keyString][cellString].built) ? '&nbsp;(' + parseInt(data.table[keyString][cellString].ms / 60000) + ':' + parseInt(data.table[keyString][cellString].ms / 1000 % 60) + ')' : '';
                         document.getElementById(keyString + '_' + cellString).innerHTML = data.table[keyString][cellString].value + buildTimeSting;
                     }
+                    //var statusStyle = ($.inArray(keyString + '_' + cellString, applicationStatus ) >= 0)?';border-right: 5px solid green;':';border-right: 5px solid grey;';
                     updateDeploymentStatus(keyString, cellString, data.table[keyString][cellString].style);
                 }
             }
@@ -165,6 +176,11 @@ function doUpdate() {
             var diskFreeValue = Math.floor((data.diskTotal - data.diskFree) / data.diskTotal * 100);
             document.getElementById('diskFree').innerHTML = diskFreeValue + '%&nbsp;disk';
             document.getElementById('diskFree').style.width = diskFreeValue + '%';
+        }
+        if (typeof data.buildHost === 'undefined' || data.buildHost === null) {
+            document.getElementById('buildLabel').innerHTML = 'Build Host Unknown';
+        } else {
+            document.getElementById('buildLabel').innerHTML = data.buildHost;
         }
         doSort();
         clearTimeout(updateTimer);
