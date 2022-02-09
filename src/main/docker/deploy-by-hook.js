694f4dbf0e8ffa5bc754b5c9b42427a1d13bcccf
Peter Withers <peter-gthb@bambooradical.com>
2022-02-09 11:08:54 +0100

    Changing the way the json_to_xml container is handled to address a posible race condition.

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -1712,7 +1712,7 @@ function convertJsonToXml() {
         + ' -Dexec.args=\\"-classpath %classpath nl.mpi.tg.eg.experimentdesigner.util.JsonToXml /FrinexBuildService/incoming/validating /FrinexBuildService/processing/validated /FrinexBuildService/listing ' + targetDirectory /* the schema file is in the target directory, however it might be nicer to use a dedicated directory when we support multiple schema/build versions */ + '\\"'
         + ' &>> ' + targetDirectory + '/json_to_xml.txt;'
         + ' chmod a+rwx -R /FrinexBuildService/processing/validated /FrinexBuildService/listing'
-        + ' &>> ' + targetDirectory + '/json_to_xml.txt;"'
+        + ' &>> ' + targetDirectory + '/json_to_xml.txt;";'
         + ' fi;';
     //+ " &> " + targetDirectory + "/JsonToXml_" + new Date().toISOString() + ".txt";
     console.log(dockerString);
