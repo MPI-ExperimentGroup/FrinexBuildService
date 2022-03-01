099c631b4c9c3b18f33bcda2d6f4ae222c7be7e3
Peter Withers <peter.withers@mpi.nl>
2022-03-01 16:40:20 +0100

    Added a build property for the JAVA memory to be used in the Docker services

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -361,7 +361,7 @@ function deployDockerService(currentEntry, warFileName, serviceName) {
     fs.writeFileSync(dockerFilePath,
         "FROM openjdk:11\n"
         + "COPY " + warFileName + " /" + warFileName + "\n"
-        + "CMD [\"java\", \"-jar\", \"-Xms" + dockerServiceMemory + "\", \"-Xmx" + dockerServiceMemory + "\", \"-XX:MaxPermSize" + dockerServiceMemory + "\", \"/" + warFileName + "\", \"--server.servlet.context-path=/" + serviceName + "\"]\n"
+        + "CMD [\"java\", \"-jar\", \"-Xms" + dockerServiceMemory + "M\", \"-Xmx" + dockerServiceMemory + "M\", \"-XX:MaxPermSize=" + dockerServiceMemory + "\", \"/" + warFileName + "\", \"--server.servlet.context-path=/" + serviceName + "\"]\n"
         // TODO: it should not be necessary to do a service start, but this needs to be tested 
         // note that manually stopping the services will cause an outage whereas replacing the service will minimise service disruption
         , { mode: 0o755 });
