faa20f34b8c6b34f7c8dafc5294c2e825387dbdc
Peter Withers <peter-gthb@bambooradical.com>
2022-03-09 13:39:28 +0100

    Adding the script to test the round trip functionality for each staging service.

--- src/main/script/frinex_service_hammer.sh
+++ src/main/script/frinex_service_hammer.sh
@@ -13,13 +13,14 @@ testUrls=$(sudo docker service ls \
     | grep -E "8080/tcp" \
     | sed 's/[*:]//g' \
     | sed 's/->8080\/tcp//g' \
-    | awk '{print "http://$swarmNode1Url:" $6 "\nhttp://$swarmNode2Url:" $6 "\nhttp://$swarmNode3Url:" $6 "\nhttps://$nginxProxiedUrl/" $2 "\n"}') \
-    | sed 's/_staging_admin/-admin/g'
+    | awk '{print "http://$swarmNode1Url:" $6 "\nhttp://$swarmNode2Url:" $6 "\nhttp://$swarmNode3Url:" $6 "\nhttps://$nginxProxiedUrl/" $2 "\n"}' \
+    | sed 's/_staging_admin/-admin/g')
 
 echo $testUrls
 
 for currentUrl in $testUrls
 do
+    echo $currentUrl
     curl -d '[{"viewDate" : "2020-02-11T11:50:57.458+0100","experimentName": "hammer_server","userId": "17033df9c23-7339-62bf-6f73-eb92","screenName": "hammer_server"},{"viewDate" : "2020-02-11T11:50:57.468+0100","experimentName": "with_hammer_server_example","userId": "17033df9c23-7339-62bf-6f73-eb92","screenName": "hammer_server"}]' -H 'Content-Type: application/json' $currentUrl/screenChange
     curl -d '[{"tagDate" : "2020-02-11T11:50:57.461+0100","experimentName": "hammer_server","userId": "17033df9c23-7339-62bf-6f73-eb92","screenName": "hammer_server","eventTag": "hammer_server","tagValue": "hammer_server","eventMs": "0"},{"tagDate" : "2020-02-11T11:51:00.197+0100","experimentName": "hammer_server","userId": "17033df9c23-7339-62bf-6f73-eb92","screenName": "hammer_server","eventTag": "hammer_server","tagValue": "hammer_server","eventMs": "0"}]' -H 'Content-Type: application/json' $currentUrl/tagEvent
     curl -d '[{"tagDate" : "2020-02-11T14:25:09.143+0100","experimentName": "hammer_server","userId": "17033cba808-3c8b-4f1d-7429-7bd6","screenName": "hammer_server","dataChannel": 0,"eventTag": "hammer_server","tagValue1": "hammer_server","tagValue2": "hammer_server","eventMs": "8999"},{"tagDate" : "2020-02-11T14:25:09.143+0100","experimentName": "hammer_server","userId": "17033cba808-3c8b-4f1d-7429-7bd6","screenName": "hammer_server","dataChannel": 0,"eventTag": "hammer_server","tagValue1": "hammer_server","tagValue2": "hammer_server","eventMs": "8999"}]' -H 'Content-Type: application/json' $currentUrl/tagPairEvent
