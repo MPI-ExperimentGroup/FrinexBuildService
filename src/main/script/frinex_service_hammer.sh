f55bee732b1144a339402c26c08f23046fcd64ac
Peter Withers <peter-gthb@bambooradical.com>
2022-03-09 13:49:06 +0100

    Adding the script to test the round trip functionality for each staging service.

--- src/main/script/frinex_service_hammer.sh
+++ src/main/script/frinex_service_hammer.sh
@@ -8,7 +8,7 @@ testUrls=$(sudo docker service ls \
     | grep -E "8080/tcp" \
     | sed 's/[*:]//g' \
     | sed 's/->8080\/tcp//g' \
-    | awk '{print "http://swarmNode1Url:" $6 "\nhttp://swarmNode2Url:" $6 "\nhttp://swarmNode3Url:" $6 "\nhttps://nginxProxiedUrl/" $2 "\n"}' \
+    | awk '{print "swarmNode1Url:" $6 "\nswarmNode2Url:" $6 "\nswarmNode3Url:" $6 "\nnginxProxiedUrl/" $2 "\n"}' \
     | sed 's/_staging_admin/-admin/g')
 
 echo $testUrls
