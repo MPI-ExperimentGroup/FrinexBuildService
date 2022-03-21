f98785ba9760da58f179de255430d1924973964d
Peter Withers <peter-gthb@bambooradical.com>
2022-03-21 16:14:05 +0100

    Adding the script to test the round trip functionality for each staging service.

--- src/main/script/hammer_services.sh
+++ src/main/script/hammer_services.sh
@@ -2,6 +2,7 @@
 
 # do not run this on a machine that is in use, this script is designed to cause load on the server and will insert records into the DB
 for i in {1..100}; do
+    echo $i
     /FrinexBuildService/log_service_hammer.sh&
     sleep 1
 done
