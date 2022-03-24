e63569193022ef34903ca73476f17dd9a55322aa
Peter Withers <peter.withers@mpi.nl>
2022-03-24 16:16:30 +0100

    Adding a munin plugin that checks the health satus of the current frinex experiment services and the proxy

---
+++ src/main/script/frinex_munin_plugin.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 Max Planck Institute for Psycholinguistics
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+#
+# @since 24 March 2022 15:39 PM (creation date)
+# @author Peter Withers <peter.withers@mpi.nl>
+#
+
+# This script checks the health satus of the current frinex experiment services and the proxy
+
+cd $(dirname "$0")
+scriptDir=$(pwd -P)
+#echo $scriptDir
+
+output_config() {
+    echo "Frinex Service Health"
+    echo "stagingTotal.label Total Staging"
+    echo "stagingHealthy.label Healthy Staging"
+    echo "stagingProxy.label Proxy Staging"
+    echo "stagingAdminTotal.label Total Admin Staging"
+    echo "stagingAdminHealthy.label Healthy Admin Staging"
+    echo "stagingAdminProxy.label Proxy Admin Staging"
+    echo "productionTotal.label Total Production"
+    echo "productionHealthy.label Healthy Production"
+    echo "productionProxy.label Proxy Production"
+    echo "productionAdminTotal.label Total Admin Production"
+    echo "productionAdminHealthy.label Healthy Admin Production"
+    echo "productionAdminProxy.label Proxy Admin Production"
+}
+
+output_values() {
+    printf "stagingTotal.label %d\n" $(number_of_services "_staging_web")
+    printf "stagingHealthy.label %d\n" $(health_of_services "_staging_web")
+    printf "stagingProxy.label %d\n" $(health_of_proxy "_staging_web")
+    printf "stagingAdminTotal.label %d\n" $(number_of_services "_staging_admin")
+    printf "stagingAdminHealthy.label %d\n" $(health_of_services "_staging_admin")
+    printf "stagingAdminProxy.label %d\n" $(health_of_proxy "_staging_admin")
+    printf "productionTotal.label %d\n" $(number_of_services "_production_web")
+    printf "productionHealthy.label %d\n" $(health_of_services "_production_web")
+    printf "productionProxy.label %d\n" $(health_of_proxy "_production_web")
+    printf "productionAdminTotal.label %d\n" $(number_of_services "_production_admin")
+    printf "productionAdminHealthy.label %d\n" $(health_of_services "_production_admin")
+    printf "productionAdminProxy.label %d\n" $(health_of_proxy "_production_admin")
+}
+
+number_of_services() {
+    docker service ls | grep -E $1 | wc -l
+}
+
+health_of_services() {
+    #TODO: health_of_services
+}
+
+health_of_proxy() {
+    #TODO: health_of_proxy
+}
+
+output_usage() {
+    printf >&2 "%s - munin plugin to show the number and health of Frinex experiment services\n" ${0##*/}
+    printf >&2 "Usage: %s [config]\n" ${0##*/}
+}
+
+case $# in
+    0)
+        output_values
+        ;;
+    1)
+        case $1 in
+            config)
+                output_config
+                ;;
+            *)
+                output_usage
+                exit 1
+                ;;
+        esac
+        ;;
+    *)
+        output_usage
+        exit 1
+        ;;
+esac
