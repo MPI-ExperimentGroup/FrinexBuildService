2f54359f1a49f1fbb30901088351ad1b94edbd3a
Peter Withers <peter-gthb@bambooradical.com>
2022-04-04 12:31:03 +0200

    Moving the production server check string to the configuration provided at build time.

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -800,9 +800,11 @@ function deployProductionGui(currentEntry, retryCounter) {
                             ' -Dexperiment.destinationServer=' + currentEntry.productionServer.replace(/^https?:\/\//, '')
                             + ' -Dexperiment.destinationServerUrl=' + currentEntry.productionServer
                             + ' -Dexperiment.groupsSocketUrl=ws://' + currentEntry.productionServer.replace(/^https?:\/\//, '')
+                            + ' -Dexperiment.productionCheckString=' + currentEntry.productionServer.replace(/^https?:\/\//, '')
                             : ' -Dexperiment.destinationServer=' + productionServer
                             + ' -Dexperiment.destinationServerUrl=' + productionServerUrl
                             + ' -Dexperiment.groupsSocketUrl=' + productionGroupsSocketUrl
+                            + ' -Dexperiment.productionCheckString=' + productionServerUrl.replace(/^https?:\/\//, '')
                         )
                         + ' -Dexperiment.isScalable=' + currentEntry.isScalable
                         + ' -Dexperiment.defaultScale=' + currentEntry.defaultScale
