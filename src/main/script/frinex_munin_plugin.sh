608041bf28acf0c1d390837492cb80475e48a542
Peter Withers <peter-gthb@bambooradical.com>
2022-04-20 11:03:17 +0200

    Tweaking the munin frinex health plugin timeout.

--- src/main/script/frinex_munin_plugin.sh
+++ src/main/script/frinex_munin_plugin.sh
@@ -130,7 +130,7 @@ output_values() {
             ;;
         *)
             cat $dataDirectory/frinex_munin_all
-            { staging_web_values & staging_admin_values & production_web_values & production_admin_values; } > $dataDirectory/frinex_munin_all&
+            { staging_web_values; staging_admin_values; production_web_values; production_admin_values; } > $dataDirectory/frinex_munin_all&
             ;;
     esac
 }
