6c8c01d1790ad64eacd20473eea1baa5bee82410
Peter Withers <peter-gthb@bambooradical.com>
2022-05-11 15:15:39 +0200

    Adding a dedicated docker container for testing build times over different container parameters.

--- src/test/run_frinex_build_test.sh
+++ src/test/run_frinex_build_test.sh
@@ -28,6 +28,6 @@ cd $(dirname "$0")
 scriptDir=$(pwd -P)
 #echo $scriptDir
 
-docker build --no-cache -f frinex_build_test.Dockerfile -t frinex_build_test:latest .
+docker build --no-cache -f frinex_stress_test.Dockerfile -t frinex_stress_test:latest .
 
-docker run  -v buildServerTarget:/FrinexBuildService/artifacts --rm -it --name frinex_build_test frinex_build_test:latest bash /FrinexBuildService/frinex_build_test.sh
+docker run  -v buildServerTarget:/FrinexBuildService/artifacts --rm -it --name frinex_stress_test frinex_stress_test:latest bash /FrinexBuildService/frinex_stress_test.sh
