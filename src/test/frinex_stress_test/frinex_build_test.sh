8f186a4042440ba06bba09cb577f4a6ef259de6c
Peter Withers <peter.withers@mpi.nl>
2022-05-16 17:32:32 +0200

    Tweaking the GTW buid time tests.

--- src/test/frinex_stress_test/frinex_build_test.sh
+++ src/test/frinex_stress_test/frinex_build_test.sh
@@ -78,7 +78,7 @@ echo $outputLogFile
 # echo "</table>" >> $outputHtmlFile
 # echo "<table border=1>" >> $outputHtmlFile
 # echo "<tr><td></td><td>12g -Xmx1g localWorkers=1</td><td>12g -Xmx1g localWorkers=2</td><td>12g -Xmx1g localWorkers=4</td><td>12g -Xmx1g localWorkers=6</td><td>12g -Xmx1g localWorkers=8</td><td>12g -Xmx1g localWorkers=10</td><td>12g -Xmx1g localWorkers=12</td></tr>" >> $outputHtmlFile
-# for settingCPU in 4 6 8 10 12
+# for settingCPU in 8 10 12
 # do
 #     echo "<tr><td>$settingCPU CPU</td>" >> $outputHtmlFile
 #     for settingWorkers in 1 2 4 6 8 10 12
@@ -120,7 +120,7 @@ echo $outputLogFile
 # echo "</table>" >> $outputHtmlFile
 echo "<table border=1>" >> $outputHtmlFile
 echo "<tr><td>online_emotions -Xmx2g</td><td>8g</td><td>16g</td><td>20g</td><td>24g</td></tr>" >> $outputHtmlFile
-for settingCPU in 8 10 12
+for settingCPU in 10 12 14 18
 do
     echo "<tr><td>$settingCPU CPU</td>" >> $outputHtmlFile
     for settingRAM in 8g 16g 20g 24g
@@ -142,7 +142,7 @@ done
 echo "</table>" >> $outputHtmlFile
 echo "<table border=1>" >> $outputHtmlFile
 echo "<tr><td>online_emotions</td><td>8g</td><td>16g</td><td>20g</td><td>24g</td></tr>" >> $outputHtmlFile
-for settingCPU in 12 14 18
+for settingCPU in 10 12 14 18
 do
     echo "<tr><td>$settingCPU CPU</td>" >> $outputHtmlFile
     for settingRAM in 8g 16g 20g 24g
