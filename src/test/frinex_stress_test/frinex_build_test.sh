3871bfabeba97b326fc814c894ca511985193b4f
Peter Withers <peter.withers@mpi.nl>
2022-05-16 15:22:54 +0200

    Tweaking the GTW buid time tests.

--- src/test/frinex_stress_test/frinex_build_test.sh
+++ src/test/frinex_stress_test/frinex_build_test.sh
@@ -34,7 +34,7 @@ outputLogFile=/FrinexBuildService/artifacts/frinex_build_test_$(date +%F_%T).txt
 echo $outputHtmlFile
 echo $outputLogFile
 
-# echo "<table border=1>" > $outputHtmlFile
+# echo "<table border=1>" >> $outputHtmlFile
 # echo "<tr><td></td><td>1g</td><td>2g</td><td>4g</td><td>6g</td><td>8g</td></tr>" >> $outputHtmlFile
 # for settingCPU in 1 2 4 6 8 10 12
 # do
@@ -55,7 +55,7 @@ echo $outputLogFile
 #     echo "</tr" >> $outputHtmlFile
 # done
 # echo "</table>" >> $outputHtmlFile
-# echo "<table border=1>" > $outputHtmlFile
+# echo "<table border=1>" >> $outputHtmlFile
 # echo "<tr><td></td><td>8g Xmx1g</td><td>8g Xmx2g</td><td>8g Xmx4g</td><td>8g Xmx6g</td><td>8g Xmx8g</td></tr>" >> $outputHtmlFile
 # for settingCPU in 1 2 4 6 8 10 12
 # do
@@ -76,7 +76,7 @@ echo $outputLogFile
 #     echo "</tr" >> $outputHtmlFile
 # done
 # echo "</table>" >> $outputHtmlFile
-# echo "<table border=1>" > $outputHtmlFile
+# echo "<table border=1>" >> $outputHtmlFile
 # echo "<tr><td></td><td>12g -Xmx1g localWorkers=1</td><td>12g -Xmx1g localWorkers=2</td><td>12g -Xmx1g localWorkers=4</td><td>12g -Xmx1g localWorkers=6</td><td>12g -Xmx1g localWorkers=8</td><td>12g -Xmx1g localWorkers=10</td><td>12g -Xmx1g localWorkers=12</td></tr>" >> $outputHtmlFile
 # for settingCPU in 4 6 8 10 12
 # do
@@ -97,7 +97,7 @@ echo $outputLogFile
 #     echo "</tr" >> $outputHtmlFile
 # done
 # echo "</table>" >> $outputHtmlFile
-# echo "<table border=1>" > $outputHtmlFile
+# echo "<table border=1>" >> $outputHtmlFile
 # echo "<tr><td>online_emotions</td><td>8g</td><td>12g</td><td>16g</td></tr>" >> $outputHtmlFile
 # for settingCPU in 8 10 12
 # do
@@ -118,7 +118,7 @@ echo $outputLogFile
 #     echo "</tr" >> $outputHtmlFile
 # done
 # echo "</table>" >> $outputHtmlFile
-echo "<table border=1>" > $outputHtmlFile
+echo "<table border=1>" >> $outputHtmlFile
 echo "<tr><td>online_emotions -Xmx2g</td><td>8g</td><td>16g</td><td>20g</td><td>24g</td></tr>" >> $outputHtmlFile
 for settingCPU in 8 10 12
 do
@@ -140,7 +140,7 @@ do
     echo "</tr" >> $outputHtmlFile
 done
 echo "</table>" >> $outputHtmlFile
-echo "<table border=1>" > $outputHtmlFile
+echo "<table border=1>" >> $outputHtmlFile
 echo "<tr><td>online_emotions</td><td>8g</td><td>16g</td><td>20g</td><td>24g</td></tr>" >> $outputHtmlFile
 for settingCPU in 12 14 18
 do
