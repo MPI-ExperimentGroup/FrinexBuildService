7cc82b8fc8f34772b8070587a9cb248e2c2e8676
Peter Withers <peter.withers@mpi.nl>
2022-05-31 18:34:46 +0200

    Adding a process to boot strap the experiment stats plugin data directly from postgres

--- src/main/stats/bootstrap_munin_statistics.sh
+++ src/main/stats/bootstrap_munin_statistics.sh
@@ -22,21 +22,21 @@
 PGPASSFILE=/FrinexBuildService/frinex_db_user_authentication
 export PGPASSFILE
 
-echo "{";
 # /frinex_munin_stats
 psql -h DatabaseStagingUrl -p DatabaseStagingPort -U frinex_staging_user -d postgres --no-align -t -c "select datname from pg_database where datistemplate = false and datname != 'postgres'" | while read -a currentexperiment ; do
-    currentExperimentUser=${currentexperiment%_db}_user
-    echo '"'$currentExperimentUser'": {'
-    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '\"firstDeploymentAccessed\":\"' || min(submit_date) || '\",' from screen_data";
-    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '\"totalDeploymentsAccessed\":\"' || count(distinct tag_value) || '\",' from tag_data where event_tag = 'compileDate'";
-    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '\"totalParticipantsSeen\":\"' || count(distinct user_id) || '\",' from participant";
-    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '\"firstParticipantSeen\":\"' || min(submit_date) || '\",' from participant";
-    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '\"lastParticipantSeen\":\"' || max(submit_date) || '\",' from participant";
-    echo '"participantsFirstAndLastSeen": [';
-    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '[\"' || min(submit_date) || '\",\"' || max(submit_date) || '\"],' from participant group by user_id order by min(submit_date) asc";
-    echo "],";
-    echo '"sessionFirstAndLastSeen": [';
-    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '[\"' || min(submit_date) || '\",\"' || max(submit_date) || '\"],' from tag_data group by user_id order by min(submit_date) asc";
-    echo "]},";
+    currentExperimentName=${currentexperiment%_db}
+    currentExperimentUser=$currentExperimentName"_user"
+    outputFile=/frinex_munin_stats/${currentExperimentName#frinex_}_staging_admin
+    echo $outputFile
+    #PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '\"firstDeploymentAccessed\":\"' || min(submit_date) || '\",' from screen_data";
+    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select 'totalDeploymentsAccessed.value ' || count(distinct tag_value) || '\",' from tag_data where event_tag = 'compileDate'" > $outputFile;
+    PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select 'totalParticipantsSeen.value ' || count(distinct user_id) || '\",' from participant" >> $outputFile;
+    #PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '\"firstParticipantSeen\":\"' || min(submit_date) || '\",' from participant";
+    #PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '\"lastParticipantSeen\":\"' || max(submit_date) || '\",' from participant";
+    #echo '"participantsFirstAndLastSeen": [';
+    #PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '[\"' || min(submit_date) || '\",\"' || max(submit_date) || '\"],' from participant group by user_id order by min(submit_date) asc";
+    #echo "],";
+    #echo '"sessionFirstAndLastSeen": [';
+    #PGPASSWORD=examplechangethis psql -h DatabaseStagingUrl -p DatabaseStagingPort -U ${currentExperimentUser} -d $currentexperiment --no-align -t -c "select '[\"' || min(submit_date) || '\",\"' || max(submit_date) || '\"],' from tag_data group by user_id order by min(submit_date) asc";
+    #echo "]},";
 done
-echo "}";
