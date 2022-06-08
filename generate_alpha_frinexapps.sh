280071abff887d12879865d705bf71a509f0af4a
Peter Withers <peter-gthb@bambooradical.com>
2022-06-08 14:20:35 +0200

    Separating the frinexapps image into single fucntion smaller images.

--- generate_alpha_frinexapps.sh
+++ generate_alpha_frinexapps.sh
@@ -34,7 +34,8 @@ else
     git pull
 
     # build the frinexapps-jdk dockerfile:
-    if docker build --no-cache -f docker/frinexapps-jdk.Dockerfile -t frinexapps-jdk:alpha . 
+    # --no-cache
+    if docker build -f docker/frinexapps-jdk.Dockerfile -t frinexapps-jdk:alpha . 
     then 
         # tag the alpha version with its own build version
         alphaVersion=$(docker run --rm -w /ExperimentTemplate/gwt-cordova frinexapps-jdk:alpha /bin/bash -c "cat /ExperimentTemplate/gwt-cordova.version")
