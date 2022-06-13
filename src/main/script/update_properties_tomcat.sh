b692949eaa11e65eb4e2ee292294863e97d315ae
Peter Withers <peter-gthb@bambooradical.com>
2022-06-13 10:02:35 +0200

    Adding a script to iterate all -admin.war and -admin.war.disabled files and updates the properties files with the new DB URL

---
+++ src/main/script/update_properties_tomcat.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 Max Planck Institute for Psycholinguistics
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+#
+# @since 13 June 2022 09:44 AM (creation date)
+# @author Peter Withers <peter.withers@mpi.nl>
+#
+
+# This script iterates all -admin.war and -admin.war.disabled files and updates the properties files with the new DB URL
+
+cd $(dirname "$0")
+scriptDir=$(pwd -P)
+#echo $scriptDir
+
+# find all admin war files 
+for foundPath in `find /srv/tomcat/webapps/ -maxdepth 1 -mindepth 1 -type f -name *-admin.war* -printf '%f\n'`
+do
+    tempPath = $foundPath.temp
+    cp $tempPath $foundPath
+    echo $tempPath;
+    unzip -p $tempPath WEB-INF/classes/application.properties
+
+    # unzip $tempPath WEB-INF/classes/application.properties -d .
+    # sed -i "s/localhost/updatedhost/g" WEB-INF/classes/application.properties
+    # zip $tempPath WEB-INF/classes/application.properties
+
+    unzip -p $tempPath WEB-INF/classes/application.properties
+done
