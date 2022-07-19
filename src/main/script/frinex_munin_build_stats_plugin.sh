d5f55ac582569a591bfd316753d9c9bc027705e3
Peter Withers <peter.withers@mpi.nl>
2022-07-19 16:49:07 +0200

    Tweaking the munin plugins to handle overlapping plugin calls due to network delays.

--- src/main/script/frinex_munin_build_stats_plugin.sh
+++ src/main/script/frinex_munin_build_stats_plugin.sh
@@ -46,7 +46,7 @@ output_config() {
 
 output_values() {
         cat $dataDirectory/build_stats_$1
-        load_build_stats $1 > $dataDirectory/build_stats_$1&
+        { load_build_stats $1 > $dataDirectory/build_stats_$1.tmp; mv -f $dataDirectory/build_stats_$1.tmp $dataDirectory/build_stats_$1 }&
 }
 
 load_build_stats() {
