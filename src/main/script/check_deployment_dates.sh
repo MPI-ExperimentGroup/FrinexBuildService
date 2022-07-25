853a76ee5a34c53f7103d142a2b1857ad22a25c2
Peter Withers <peter.withers@mpi.nl>
2022-07-25 15:42:15 +0200

    Preventing incomplete results being return by tomcat for the known sleepers and known experiments.

--- src/main/script/check_deployment_dates.sh
+++ src/main/script/check_deployment_dates.sh
@@ -83,15 +83,18 @@ sleepingCounter=$(ls -l /srv/tomcat/webapps/*-admin.war.disabled | wc -l)
 echo "$runningCounter experiments running" >> $scriptDir/check_deployment_dates_$(date +%F).log
 echo "$sleepingCounter experiments sleeping" >> $scriptDir/check_deployment_dates_$(date +%F).log
 
-# sudo sudo touch /srv/tomcat/webapps/ROOT/known_sleepers.json
-# sudo chown tomcat:tomcat /srv/tomcat/webapps/ROOT/known_sleepers.json
-# sudo chmod a+w /srv/tomcat/webapps/ROOT/known_sleepers.json
-echo "{" > /srv/tomcat/webapps/ROOT/known_sleepers.json
-find /srv/tomcat/webapps/ -maxdepth 1 -mindepth 1 -type f -name *-admin.war.disabled -printf '"%f,' | sed "s/-admin.war.disabled/\"/g" | sed 's/,$/\n/' | sed 's/,/,\n/g' >> /srv/tomcat/webapps/ROOT/known_sleepers.json
-echo "}" >> /srv/tomcat/webapps/ROOT/known_sleepers.json
-echo "{" > /srv/tomcat/webapps/ROOT/running_experiments.json
-find /srv/tomcat/webapps/ -maxdepth 1 -mindepth 1 -type f -name *-admin.war -printf '"%f,' | sed "s/-admin.war/\"/g" | sed 's/,$/\n/' | sed 's/,/,\n/g' >> /srv/tomcat/webapps/ROOT/running_experiments.json
-echo "}" >> /srv/tomcat/webapps/ROOT/running_experiments.json
+# sudo sudo touch /srv/tomcat/webapps/ROOT/known_sleepers.json.tmp
+# sudo chown tomcat:tomcat /srv/tomcat/webapps/ROOT/known_sleepers.json.tmp
+# sudo chmod a+w /srv/tomcat/webapps/ROOT/known_sleepers.json.tmp
+echo "{" > /srv/tomcat/webapps/ROOT/known_sleepers.json.tmp
+find /srv/tomcat/webapps/ -maxdepth 1 -mindepth 1 -type f -name *-admin.war.disabled -printf '"%f,' | sed "s/-admin.war.disabled/\"/g" | sed 's/,$/\n/' | sed 's/,/,\n/g' >> /srv/tomcat/webapps/ROOT/known_sleepers.json.tmp
+echo "}" >> /srv/tomcat/webapps/ROOT/known_sleepers.json.tmp
+echo "{" > /srv/tomcat/webapps/ROOT/running_experiments.json.tmp
+find /srv/tomcat/webapps/ -maxdepth 1 -mindepth 1 -type f -name *-admin.war -printf '"%f,' | sed "s/-admin.war/\"/g" | sed 's/,$/\n/' | sed 's/,/,\n/g' >> /srv/tomcat/webapps/ROOT/running_experiments.json.tmp
+echo "}" >> /srv/tomcat/webapps/ROOT/running_experiments.json.tmp
+
+mv -f /srv/tomcat/webapps/ROOT/known_sleepers.json.tmp /srv/tomcat/webapps/ROOT/known_sleepers.json
+mv -f /srv/tomcat/webapps/ROOT/running_experiments.json.tmp /srv/tomcat/webapps/ROOT/running_experiments.json
 
 date >> $scriptDir/check_deployment_dates_$(date +%F).log
 rm $lockFile
