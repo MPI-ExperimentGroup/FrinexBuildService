b78dd3ff59141bb7fa76cfdc0d85a4279ece8d4e
Peter Withers <peter-gthb@bambooradical.com>
2022-08-31 14:05:54 +0200

    Addressing redirect issues when behind the NGINX proxy

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -441,7 +441,7 @@ function deployDockerService(currentEntry, warFileName, serviceName) {
     fs.writeFileSync(dockerFilePath,
         "FROM openjdk:11\n"
         + "COPY " + warFileName + " /" + warFileName + "\n"
-        + "CMD [\"java\", \"-jar\", \"/" + warFileName + "\", \"--server.servlet.context-path=/" + serviceName + "\", \"--server.forward-headers-strategy=FRAMEWORK\"]\n"
+        + "CMD [\"java\", \"-jar\", \"/" + warFileName + "\", \"--server.servlet.context-path=/" + serviceName + "\", \"--server.forward-headers-strategy=FRAMEWORK\", \"--trace\"]\n"
         // + "CMD [\"java\", \"-jar\", \"/" + warFileName + "\", \"--server.servlet.context-path=/" + currentEntry.buildName + '-admin' + "\"]\n"
         // TODO: it should not be necessary to do a service start, but this needs to be tested 
         // note that manually stopping the services will cause an outage whereas replacing the service will minimise service disruption
