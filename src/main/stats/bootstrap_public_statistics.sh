91c67a2873ba140a25b0a2068c8b60424d5c23c0
Peter Withers <peter-gthb@bambooradical.com>
2022-09-28 16:43:28 +0200

    Updating the public stats bootstrap script to include the versions of Frinex and dates used etc.

--- src/main/stats/bootstrap_public_statistics.sh
+++ src/main/stats/bootstrap_public_statistics.sh
@@ -22,35 +22,35 @@
 PGPASSFILE=/FrinexBuildService/frinex_db_user_authentication
 export PGPASSFILE
 
-postgresCommand="psql -h DatabaseStagingUrl -p DatabaseStagingPort -d"
+postgresCommand="psql -h DatabaseStagingUrl -p DatabaseStagingPort"
 # postgresCommand="/Applications/Postgres.app/Contents/Versions/9.4/bin/psql -p5432"
 
 echo "{";
 isFirstEntry=true;
 # $postgresCommand postgres --no-align -t -c "select datname from pg_database where datistemplate = false and datname != 'postgres' and datname like 'frinex_%_db'" | while read -a currentexperiment ; do
-for currentexperiment in $($postgresCommand -U db_manager_frinex_staging postgres --no-align -t -c "select datname from pg_database where datistemplate = false and datname != 'postgres' and datname like 'frinex_%_db'");
+for currentexperiment in $($postgresCommand -U db_manager_frinex_staging -d postgres --no-align -t -c "select datname from pg_database where datistemplate = false and datname != 'postgres' and datname like 'frinex_%_db'");
 do 
     if [ "$isFirstEntry" = false ] ; then
       echo "},";
     fi
     isFirstEntry=false;
-    echo '"'$currentexperiment'": {'
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"firstDeploymentAccessed\":\"' || min(submit_date) || '\",' from screen_data";
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"totalDeploymentsAccessed\":\"' || count(distinct tag_value) || '\",' from tag_data where event_tag = 'compileDate'";
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"totalParticipantsSeen\":\"' || count(distinct user_id) || '\",' from participant";
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"firstParticipantSeen\":\"' || min(submit_date) || '\",' from participant";
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"lastParticipantSeen\":\"' || max(submit_date) || '\",' from participant";
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"totalPageLoads\":\"' || count(distinct tag_date) || '\",' from tag_data where event_tag = 'compileDate'";
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"totalStimulusResponses\":\"' || count(distinct concat(tag_date, user_id, event_ms)) || '\",' from stimulus_response";
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"totalMediaResponses\":\"' || count(id) || '\",' from audio_data";
+    echo '"'${currentexperiment%_db}'": {'
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"firstDeploymentAccessed\":\"' || min(submit_date) || '\",' from screen_data";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"totalDeploymentsAccessed\":\"' || count(distinct tag_value) || '\",' from tag_data where event_tag = 'compileDate'";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"totalParticipantsSeen\":\"' || count(distinct user_id) || '\",' from participant";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"firstParticipantSeen\":\"' || min(submit_date) || '\",' from participant";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"lastParticipantSeen\":\"' || max(submit_date) || '\",' from participant";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"totalPageLoads\":\"' || count(distinct tag_date) || '\",' from tag_data where event_tag = 'compileDate'";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"totalStimulusResponses\":\"' || count(distinct concat(tag_date, user_id, event_ms)) || '\",' from stimulus_response";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"totalMediaResponses\":\"' || count(id) || '\",' from audio_data";
     echo '"participantsFirstAndLastSeen": [';
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '[\"' || min(submit_date) || '\",\"' || max(submit_date) || '\"],' from participant where submit_date is not null group by user_id order by min(submit_date) asc" | sed "$ s|\],[[:space:]]*$|]|g";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '[\"' || min(submit_date) || '\",\"' || max(submit_date) || '\"],' from participant where submit_date is not null group by user_id order by min(submit_date) asc" | sed "$ s|\],[[:space:]]*$|]|g";
     echo "],";
     echo '"sessionFirstAndLastSeen": [';
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '[\"' || min(submit_date) || '\",\"' || max(submit_date) || '\"],' from tag_data where submit_date is not null group by user_id order by min(submit_date) asc" | sed "$ s|\],[[:space:]]*$|]|g";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '[\"' || min(submit_date) || '\",\"' || max(submit_date) || '\"],' from tag_data where submit_date is not null group by user_id order by min(submit_date) asc" | sed "$ s|\],[[:space:]]*$|]|g";
     echo "],";
     echo '"frinexVersion": {';
-    PGPASSWORD='DatabaseStagingPass' $postgresCommand $currentexperiment -U ${currentexperiment%_db} --no-align -t -c "select '\"' || tag_value || '\": { \"first_use\": \"' || min(tag_date) || '\", \"last_use\": \"' || max(tag_date) || '\", \"page_loads\": ' || count(tag_value) || ', \"distinct_users\": ' || count(distinct(user_id)) || '},'  from tag_data where event_tag = 'projectVersion' group by tag_value order by min(tag_value) asc" | sed "$ s|\},[[:space:]]*$|}|g";
+    PGPASSWORD='DatabaseStagingPass' $postgresCommand -U ${currentexperiment%_db}"_user" -d $currentexperiment --no-align -t -c "select '\"' || tag_value || '\": { \"first_use\": \"' || min(tag_date) || '\", \"last_use\": \"' || max(tag_date) || '\", \"page_loads\": ' || count(tag_value) || ', \"distinct_users\": ' || count(distinct(user_id)) || '},'  from tag_data where event_tag = 'projectVersion' group by tag_value order by min(tag_value) asc" | sed "$ s|\},[[:space:]]*$|}|g";
     echo "}";
 done
 echo "}";
