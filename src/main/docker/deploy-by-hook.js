325c85d790d8be588c216d5c8d3ed591452cc6bb
Peter Withers <peter.withers@mpi.nl>
2022-11-17 15:36:20 +0100

    Adding a target staging server option to the XML to work around the nginx proxy issues for websockets, webrtc and android apps on the internal wifi etc.

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -534,9 +534,18 @@ function deployStagingGui(currentEntry) {
             + ' -Dexperiment.configuration.path=/FrinexBuildService/processing/staging-building'
             + ' -DversionCheck.allowSnapshots=' + 'false'
             + ' -DversionCheck.buildType=' + 'stable'
-            + ' -Dexperiment.destinationServer=' + stagingServer
-            + ' -Dexperiment.destinationServerUrl=' + stagingServerUrl
-            + ' -Dexperiment.groupsSocketUrl=' + stagingGroupsSocketUrl
+            // TODO: usage of currentEntry.stagingServer is not implemented for targeting tomcat deployments nor undeployments, it is currently only used to tell the GUI application where its URL is for websockets and mobile apps
+            + ((currentEntry.stagingServer != null && currentEntry.stagingServer.length > 0) ?
+                ' -Dexperiment.destinationServer=' + currentEntry.stagingServer.replace(/^https?:\/\//, '')
+                + ' -Dexperiment.destinationServerUrl=' + currentEntry.stagingServer
+                + ' -Dexperiment.groupsSocketUrl=ws://' + currentEntry.stagingServer.replace(/^https?:\/\//, '')
+                : ' -Dexperiment.destinationServer=' + stagingServer
+                + ' -Dexperiment.destinationServerUrl=' + stagingServerUrl
+                + ' -Dexperiment.groupsSocketUrl=' + stagingGroupsSocketUrl
+            )
+            // + ' -Dexperiment.destinationServer=' + stagingServer 
+            // + ' -Dexperiment.destinationServerUrl=' + stagingServerUrl 
+            // + ' -Dexperiment.groupsSocketUrl=' + stagingGroupsSocketUrl
             + ' -Dexperiment.isScalable=' + currentEntry.isScalable
             + ' -Dexperiment.defaultScale=' + currentEntry.defaultScale
             + ' -Dexperiment.configuration.defaultLocale=' + currentEntry.defaultLocale
