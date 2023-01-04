7681233b93867265e812486c7c05035b820f8031
Peter Withers <peter-gthb@bambooradical.com>
2023-01-04 16:10:19 +0100

    Adding a service to test the load handling capacity of the given server

--- src/test/frinex_stress_test/load_test.sh
+++ src/test/frinex_stress_test/load_test.sh
@@ -4,8 +4,8 @@ date
 for i in {1..600}; do
     # one participant per second for 10 minutes 60x10, which if completed in that 10 minutes should be a similar rate to 86400 participants in 24 hours
     echo $i
-    time /frinex_load_test/load_participant.sh&
-    sleep 1
-    pidof load_participant.sh | wc -w
-    pidof curl | wc -w
+    time /frinex_load_test/load_participant.sh
+    # sleep 1
+    # pidof load_participant.sh | wc -w
+    # pidof curl | wc -w
 done
