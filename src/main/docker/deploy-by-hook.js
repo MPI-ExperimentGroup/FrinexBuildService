eeeb18c1c3e80a465dd0b448a72a40aa1256048c
Peter Withers <peter-gthb@bambooradical.com>
2023-03-22 12:59:00 +0100

    Setting the XPath limit for the unit tests run on alloptions when creating the alpha JDK image

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -1996,6 +1996,7 @@ function convertJsonToXml() {
         + ' frinexapps-jdk:alpha /bin/bash -c "mvn exec:exec'
         + ' -gs /maven/.m2/settings.xml'
         + ' -DskipTests'
+        + ' -Djdk.xml.xpathExprGrpLimit=140 -Djdk.xml.xpathExprOpLimit=650 -Djdk.xml.xpathTotalOpLimit=150'
         + ' -Dlog4j2.version=2.17.2'
         + ' -Dexec.executable=java'
         + ' -Dexec.classpathScope=runtime'
