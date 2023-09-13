2cbfe273da42cf1c629c1b07f683143e4c924fdd
Peter Withers <peter-gthb@bambooradical.com>
2023-09-13 13:28:51 +0200

    Adding a system for the Frinex examples  to be automatically build by the Frinex build system when the alpha build image is generated

--- update_frinex_examples.sh
+++ update_frinex_examples.sh
@@ -44,9 +44,9 @@ else
         cp -rfu /FrinexBuildService/git-checkedout/frinex_examples/$configFile/ /FrinexBuildService/incoming/commits/; \
     done; \
     chmod -R a+rw /FrinexBuildService/incoming/commits; \
-    echo "frinex_examples:"; \
+    echo 'frinex_examples:'; \
     ls -l /FrinexBuildService/git-checkedout/frinex_examples/; \
-    echo "commits:"; \
-    ls -l /FrinexBuildService/incoming/commits/;" \";
+    echo 'commits:'; \
+    ls -l /FrinexBuildService/incoming/commits/;";
     echo "frinex_examples ok, to trigger the examples to be built browse to /cgi/request_build.cgi, requires log in."
 fi;
