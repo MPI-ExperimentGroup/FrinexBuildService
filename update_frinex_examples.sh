63f998e6a755994a0864a33acd50d75a5332d0b2
Peter Withers <peter-gthb@bambooradical.com>
2023-09-13 16:42:29 +0200

    Adding a system for the Frinex examples  to be automatically build by the Frinex build system when the alpha build image is generated

--- update_frinex_examples.sh
+++ update_frinex_examples.sh
@@ -36,7 +36,7 @@ else
     docker run --rm -v gitCheckedout:/FrinexBuildService/git-checkedout -v incomingDirectory:/FrinexBuildService/incoming -w /ExperimentTemplate frinexapps-jdk:alpha /bin/bash -c "\
     git pull --no-rebase; \
     mkdir -p /FrinexBuildService/git-checkedout/frinex_examples/; \
-    for configFile in \$(diff -q /ExperimentTemplate/ExperimentDesigner/src/main/resources/examples/ /FrinexBuildService/git-checkedout/frinex_examples/ | grep -Ei '.xml' | sed -e 's_.*ExperimentDesigner/src/main/resources/examples/__g' | sed -e 's/.xml .*//g'); \
+    for configFile in \$(diff -q /ExperimentTemplate/ExperimentDesigner/src/main/resources/examples/ /FrinexBuildService/git-checkedout/frinex_examples/ | grep -Ei '.xml' | sed -e 's_.*ExperimentDesigner/src/main/resources/examples/__g' | sed -e 's/.xml.*//g' | sed -e 's/[^a-z0-9_]//g'); \
     do \
         echo \$configFile.xml
         cp -rfu /ExperimentTemplate/ExperimentDesigner/src/main/resources/examples/\$configFile.xml /FrinexBuildService/git-checkedout/frinex_examples/; \
