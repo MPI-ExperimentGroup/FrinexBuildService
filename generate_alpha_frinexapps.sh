b42a8506802d9026daf6f6631460cfd3ffc56d9a
Peter Withers <peter-gthb@bambooradical.com>
2024-09-23 12:45:21 +0200

    updating comments regarding the local maven directory

--- generate_alpha_frinexapps.sh
+++ generate_alpha_frinexapps.sh
@@ -53,7 +53,7 @@ else
         # copy the maven settings to the .m2 directory that is a in volume
         cat $workingDir/src/main/config/settings.xml | docker run -v m2Directory:/maven/.m2/ -i frinexapps-jdk:alpha /bin/bash -c 'cat > /maven/.m2/settings.xml'
 
-        # make sure the local .m2 directory has the alpha jar files. In this case we just install AdaptiveVocabularyAssessmentModule which will also install frinex common and the parent pom, because compiling the GWT component is not needed here
+        # make sure the local .m2 directory has the alpha jar files. In this case only frinex common and the parent pom are required, compiling the GWT component is not needed here
         docker run --rm -v m2Directory:/maven/.m2/ -w /ExperimentTemplate frinexapps-jdk:alpha /bin/bash -c "mvn install -Djdk.xml.xpathExprGrpLimit=140 -Djdk.xml.xpathExprOpLimit=650 -Djdk.xml.xpathTotalOpLimit=150 -gs /maven/.m2/settings.xml"
         echo "frinexapps-jdk ok"
 
