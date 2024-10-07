05c288fbd715d5680b0c0fdfd3a35a684cd11762
Peter Withers <peter-gthb@bambooradical.com>
2024-10-07 12:14:59 +0200

    Adding a service to run on all nodes and keep the local images on each node in sync with the running experiment services

---
+++ src/main/docker/frinex_synchronisation_service.Dockerfile
@@ -0,0 +1,32 @@
+# frinex_synchronisation_service
+# @since 07 October 2024 11:36 PM (creation date)
+# @author Peter Withers <peter.withers@mpi.nl>
+FROM alpine:3.14
+RUN apk add --no-cache \
+  curl \
+  coreutils \
+  bash \
+  rsync \
+  docker \
+  sudo
+RUN mkdir /FrinexBuildService/
+COPY config/publish.properties /FrinexBuildService/
+COPY script/frinex_synchronisation_service.sh /FrinexBuildService/
+RUN dockerRegistry=$(grep dockerRegistry /FrinexBuildService/publish.properties | sed "s/dockerRegistry[ ]*=[ ]*//g" | tr -d "\n" | tr -d "\r"); sed -i "s/DOCKER_REGISTRY/$dockerRegistry/g" /FrinexBuildService/frinex_synchronisation_service.sh
+RUN rm /FrinexBuildService/publish.properties
+
+RUN echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/docker push [a-zA-Z0-9-_.]*/[a-z0-9-_]*_staging_web\:[0-9]*' >> /etc/sudoers
+RUN echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/docker push [a-zA-Z0-9-_.]*/[a-z0-9-_]*_staging_admin\:[0-9]*' >> /etc/sudoers
+RUN echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/docker push [a-zA-Z0-9-_.]*/[a-z0-9-_]*_production_web\:[0-9]*' >> /etc/sudoers
+RUN echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/docker push [a-zA-Z0-9-_.]*/[a-z0-9-_]*_production_admin\:[0-9]*' >> /etc/sudoers
+
+RUN echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/docker system prune -f' >> /etc/sudoers
+RUN echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/docker service ls' >> /etc/sudoers
+RUN echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/docker service ls --format {{.Name}}' >> /etc/sudoers
+
+RUN adduser -S frinex
+RUN chown -R frinex /FrinexBuildService
+RUN chmod -R ug+rwx /FrinexBuildService
+WORKDIR /FrinexBuildService
+USER frinex
+ENTRYPOINT ["/FrinexBuildService/frinex_synchronisation_service.sh"]
---
+++ src/main/script/frinex_synchronisation_service.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Copyright (C) 2024 Max Planck Institute for Psycholinguistics
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+# @since 07 October 2024 11:36 PM (creation date)
+# @author Peter Withers <peter.withers@mpi.nl>
+
+# this script runs as a service on each swarm node and makes sure that the 
+# images used by each experiment service is available locally should the 
+# registry become unavailable or be moved to a differnent node. Any image
+# that is not used in a service will be cleaned to save disk space. Any 
+# image that is local and used by a service but not in the registry will
+# be pushed to the registry for other nodes to access.
+while true
+do
+    echo "DOCKER_REGISTRY"
+    # echo all arguments
+    printf '%s\n' "$*"
+    
+    # Add a service to run on all nodes, when a service is running where the image is not local then pull that image
+    # When the registry start up, push all images for running services to the freshly started registry
+    # On each node a clean can be run and all images not found in the running services can be deleted
+
+    # 1009  curl -k https://frinexbuild.mpi.nl:443/v2/_catalog?n=1000
+    # 1010  curl -k https://frinexbuild.mpi.nl:443/v2/very_large_example_staging_admin/tags/list
+
+    for currentServiceImage in $(sudo docker service ls | grep -E "_staging_web|_production_web|_staging_admin|_production_admin" | awk '{print $5}')
+    do
+        echo $currentServiceImage
+    done
+    sleep 1h
+done
