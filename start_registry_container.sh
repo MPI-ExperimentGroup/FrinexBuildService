78367beb719315746274224c291fe99feb120315
Peter Withers <peter-gthb@bambooradical.com>
2024-10-10 11:43:07 +0200

    Adding a service to run on all nodes and keep the local images on each node in sync with the running experiment services

--- start_registry_container.sh
+++ start_registry_container.sh
@@ -40,7 +40,7 @@ docker service rm registry
 # docker volume rm frinexDockerRegistry
 docker service create -d \
    --name registry \
-   -v frinexDockerRegistry:/var/lib/registry \
+   --mount type=volume,src=frinexDockerRegistry,dst=/var/lib/registry \
    --replicas=1 \
    --secret frinexbuild.mpi.nl.crt \
    --secret frinexbuild.mpi.nl.key \
