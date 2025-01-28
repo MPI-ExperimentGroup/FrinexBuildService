008b5b0bcf69a5306e5f85d5de0724fe01bbdfc4
Peter Withers <peter-gthb@bambooradical.com>
2025-01-28 11:20:15 +0100

    adding sshd to the synchronisation service and testing the synchronisation with the build history file

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -2158,7 +2158,7 @@ function convertJsonToXml() {
         + ' -Dexec.args=\\"-classpath %classpath nl.mpi.tg.eg.experimentdesigner.util.JsonToXml /FrinexBuildService/incoming/prevalidation /FrinexBuildService/incoming/postvalidation /FrinexBuildService/listing ' + targetDirectory /* the schema file is in the target directory, however it might be nicer to use a dedicated directory when we support multiple schema/build versions */ + '\\"'
         + ' &> ' + targetDirectory + '/json_to_xml.txt;'
         + ' chmod a+rwx -R /FrinexBuildService/incoming/postvalidation'
-        + ' &>> ' + targetDirectory + '/json_to_xml.txt;";'
+        + ' &>> ' + targetDirectory + '/json_to_xml.txt;'
         + ' mv /FrinexBuildService/incoming/postvalidation/* /FrinexBuildService/processing/validated/'
         + ' &>> ' + targetDirectory + '/json_to_xml.txt;'
         + ' chmod a+rwx -R /FrinexBuildService/processing/validated /FrinexBuildService/listing'
