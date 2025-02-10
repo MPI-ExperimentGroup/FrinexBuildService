f1e1bbeae1b38809ddfffd39b26a4ef28b2b7f17
Peter Withers <peter-gthb@bambooradical.com>
2025-02-10 13:11:42 +0100

    adding sshd to the synchronisation service and testing the synchronisation with the build history file

---
+++ src/main/script/sync_diff_experiment_swarm_nodes.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# @since 10 February 2025 12:26 PM (creation date)
+# @author Peter Withers <peter.withers@mpi.nl>
+#
+
+# This script diffs the files for a given experiment on all nodes in the docker swarm
+
+buildName=$1
+if [[ "$buildName" == "" ]]; then
+    echo "buildName: $buildName is required"
+else if [ ! -d "/FrinexBuildService/artifacts/$buildName" ]; then
+        echo "directory not found /FrinexBuildService/artifacts/$buildName"
+    else
+        for servicePortAndNode in $(sudo docker service ls --format "{{.Ports}}{{.Name}}" -f "name=frinex_synchronisation_service" | sed 's/[*:]//g' | sed 's/->22\/tcp//g')
+        do
+            servicePort=$(echo $servicePortAndNode | sed 's/frinex_synchronisation_service_[a-zA-Z0-9]*//g')
+            nodeName=$(echo $servicePortAndNode | sed 's/[0-9]*frinex_synchronisation_service_//g')
+            echo "buildName: $buildName"
+            echo "nodeName: $nodeName"
+            echo "servicePort: $servicePort"
+
+            # output a debuging diff of the local and renote files
+            ls -lG /FrinexBuildService/protected/$buildName > /FrinexBuildService/artifacts/$buildName/listingLocal.txt;
+            ls -lG /FrinexBuildService/artifacts/$buildName >> /FrinexBuildService/artifacts/$buildName/listingLocal.txt;
+            ssh $nodeName.mpi.nl -p $servicePort "ls -lG /FrinexBuildService/protected/$buildName; ls -lG /FrinexBuildService/artifacts/$buildName" > /FrinexBuildService/artifacts/$buildName/listing$servicePort.txt; 
+            diff --ignore-space-change -U 0 /FrinexBuildService/artifacts/$buildName/listingLocal.txt /FrinexBuildService/artifacts/$buildName/listing$servicePort.txt
+        done
+    fi
+fi
