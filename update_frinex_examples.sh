3b7a41386cee210b8a60d92b0c9a2232f5e39abf
Peter Withers <peter-gthb@bambooradical.com>
2025-06-23 12:19:42 +0200

    Creating the frinex_examples directory with the correct permissions

--- update_frinex_examples.sh
+++ update_frinex_examples.sh
@@ -33,9 +33,11 @@ else
     echo "update requested for $1 examples"
     # TODO: check for changes and always update examples using $1 version
     # make sure frinex_examples exists and has the correct permission 
-    docker run --rm -v gitCheckedout:/FrinexBuildService/git-checkedout frinexbuild:latest /bin/bash -c "mkdir -p /FrinexBuildService/git-checkedout/frinex_examples/;"
+    docker run --rm -v gitCheckedout:/FrinexBuildService/git-checkedout frinexbuild:latest /bin/bash -c "mkdir -p /FrinexBuildService/git-checkedout/frinex_examples; chmod -R ug+rwx /FrinexBuildService/git-checkedout/frinex_examples"
     # update the frinex_examples
     docker run --rm -v gitCheckedout:/FrinexBuildService/git-checkedout -v incomingDirectory:/FrinexBuildService/incoming -w /ExperimentTemplate frinexapps-jdk:alpha /bin/bash -c "\
+    whoami; \
+    stat /FrinexBuildService/git-checkedout/frinex_examples/ \
     git checkout ExperimentDesigner/pom.xml; git pull --no-rebase; \
     mkdir -p /FrinexBuildService/git-checkedout/frinex_examples/; \
     for configFile in \$(diff -q /ExperimentTemplate/ExperimentDesigner/src/main/resources/examples/ /FrinexBuildService/git-checkedout/frinex_examples/ | grep -Ei '.xml' | sed -e 's_.*ExperimentDesigner/src/main/resources/examples/__g' | sed -e 's/.xml.*//g' | sed -e 's/[^a-z0-9_]//g'); \
