55b94a853ac3f0786c18570ee9bdbdeeb412192e
Peter Withers <peter.withers@mpi.nl>
2025-09-23 14:59:28 +0200

    Adding a security group that determines which which ActiveDirectoy group membership of is required for access to the administration system. Auditing all endpoints

--- src/main/docker/deploy-by-hook.js
+++ src/main/docker/deploy-by-hook.js
@@ -468,8 +468,9 @@ function deployDockerService(currentEntry, warFileName, serviceName, contextPath
         + "COPY " + warFileName + " /" + warFileName + "\n"
         // + "CMD [\"java\", \"-jar\", \"/" + warFileName + "\", \"--server.servlet.context-path=/" + contextPath + "\""
         + "CMD [\"java\", \"-jar\", \"/" + warFileName + "\", \"--server.servlet.context-path=/" + contextPath + "\", \"--server.forward-headers-strategy=FRAMEWORK\""
-        + ((currentEntry.state === "debug") ? ", \"--trace\", \"-Dspring.jpa.show-sql=true\", \"-Dspring.jpa.properties.hibernate.format_sql=true\", \"-Dmanagement.endpoints.web.exposure.include=mappings\", \"-Dmanagement.endpoint.mappings.enabled=true\"]\n" : "]\n")
+        + ((currentEntry.state === "debug") ? ", \"--trace\", \"-Dspring.jpa.show-sql=true\", \"-Dspring.jpa.properties.hibernate.format_sql=true\"]\n" : "]\n")
         // exposure.include=mappings and mappings.enabled enables /actuator/mappings
+        // , \"-Dmanagement.endpoints.web.exposure.include=mappings\", \"-Dmanagement.endpoint.mappings.enabled=true\"
         // TODO: it should not be necessary to do a service start, but this needs to be tested 
         // note that manually stopping the services will cause an outage whereas replacing the service will minimise service disruption
         , { mode: 0o775 });
