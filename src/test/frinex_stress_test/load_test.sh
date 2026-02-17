10b841076454a8c041bcbb9a862090766cf39f75
Peter Withers <peter.withers@mpi.nl>
2026-02-17 18:01:15 +0100

    Reducing the concurrent simulated participants to 1000 per 20 minute period

--- src/test/frinex_stress_test/load_test.sh
+++ src/test/frinex_stress_test/load_test.sh
@@ -3,7 +3,7 @@ date
 currentUrl="$1"
 echo "currentUrl: $currentUrl"
 # do not run this on a machine that is in use, this script is designed to cause load on the server and will insert records into the DB
-for i in $(seq 1 20); do
+for i in $(seq 1 10); do
     # one participant per second for 10 minutes 60x10, which if completed in that 10 minutes should be a similar rate to 86400 participants in 24 hours
     echo $i
     /frinex_load_test/load_participant.sh "$currentUrl" > "load_participant_${i}.log" 2>&1 &
