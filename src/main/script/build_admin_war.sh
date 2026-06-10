c790d685bd5b6788a108a06e5b28d33c71c00d2f
Peter Withers <peter-gthb@bambooradical.com>
2026-06-10 11:32:34 +0200

    Setting up to trigger admin jar rebuilds and testing whcih admin version is active

--- src/main/script/build_admin_war.sh
+++ src/main/script/build_admin_war.sh
@@ -13,7 +13,16 @@ fi
 buildContainerOptions=$(grep buildContainerOptions /FrinexBuildService/publish.properties | sed "s/buildContainerOptions[ ]*=[ ]*//g" | tr -d "\n" | tr -d "\r");
 buildContainerName="$cleanedInput"
 buildName=$(echo "$cleanedInput" | sed -E 's/_(staging|production)_(web|admin)$//')
-frinexVersion="admin-stable"
+frinexVersion="admin-stable" #            + ((currentEntry.frinexVersion === "alpha") ? "alpha" : 'admin-stable')
+
+#TODO:const configServer = properties.webservice.configServer;
+#TODO:const stagingServer = properties.staging.serverName;
+#TODO:const stagingServerUrl = properties.staging.serverUrl;
+#TODO:const stagingGroupsSocketUrl = properties.staging.groupsSocketUrl;
+#TODO:const stagingDbHost = properties.staging.dbHost;
+#TODO: allowDelete=allowDelete=' + ((currentEntry.allowDelete != null) ? currentEntry.allowDelete : 'false')
+#TODO: securityGroup=("_security_group_", currentEntry.securityGroup ?? ''), { stdio: [0, 1, 2] });
+
 
 echo "cleanedInput: $cleanedInput"
 echo "buildContainerOptions: $buildContainerOptions"
@@ -21,68 +30,79 @@ echo "buildContainerName: $buildContainerName"
 echo "buildName: $buildName"
 echo "frinexVersion: $frinexVersion"
 
-# TODO: convert and validate the following build process
-exit 0
-
             # // + ((["load_test_target", "with_stimulus_example", "thijs_test_3"].includes(buildName)) ? 'admin-beta' : ((currentEntry.frinexVersion != null && currentEntry.frinexVersion.length > 0) ? currentEntry.frinexVersion : 'stable'))
             # + ((["load_test_target", "with_stimulus_example", "thijs_test_3"].includes(buildName)) ? 'admin-beta' : 'admin-stable')
 
 # /* not currently required */ //+ ' --net="host" ' // enables the container to connect to ports on the host, so that maven can access tomcat manager
 #             // # the maven settings and its .m2 directory need to be in the volume m2Directory:/maven/.m2/
             
-sudo docker run --rm $buildContainerOptions --name $buildContainerName \
-            -v processingDirectory:/FrinexBuildService/processing \
-            -v buildServerTarget:/FrinexBuildService/artifacts \
-            -v protectedDirectory:/FrinexBuildService/protected \
-            -v m2Directory:/maven/.m2/ \
-            -w /ExperimentTemplate frinexapps-jdk:$frinexVersion \
-            /bin/bash -c "cd /ExperimentTemplate/registration; \
-            // using sed to replace the destinationServerUrl with destinationServer for older build images, new build images did not need this but since the addition of the proxy it is now required for all
-            sed -i 's|>\\${experiment.destinationServer}/manager/text|>https://\\${experiment.destinationServer}/manager/text|g' /ExperimentTemplate/pom.xml; \
-            sed -i 's|>\\${experiment.destinationServerUrl}/manager/text|>https://\\${experiment.destinationServer}/manager/text|g' /ExperimentTemplate/pom.xml; \
-            mkdir /FrinexBuildService/artifacts/${buildName}/included_artifacts/;'
-            // + ' ls -l /FrinexBuildService/artifacts/${buildName} &>> /FrinexBuildService/artifacts/${buildName}/${buildName}_staging_admin.txt;'
-            + ' mvn clean compile ' // the target 'compile' is used to cause compilation errors to show up before all the effort/time of the full build process
-            + ((/* currentEntry.isWebApp && isWebApp is incorrect, non web apps still need the admin */ deploymentType.includes('staging_tomcat') || ( /* limiting tomcat deployments to when a server is specified */ currentEntry.stagingServer != null && currentEntry.stagingServer.length > 0)) ? 'tomcat7:undeploy tomcat7:redeploy' : 'package')
-            //+ 'package'
-            + ' -gs /maven/.m2/settings.xml'
-            + ' -DskipTests'
-            // + ' -Dlog4j2.version=2.17.2'
-            //+ ' -pl frinex-admin'
-            + ' -q' // quiet output
-            + ' -Dexperiment.configuration.name=${buildName}
-            + ' -Dexperiment.configuration.displayName=\\\"' + currentEntry.experimentDisplayName + '\\\"'
-            + ' -Dexperiment.webservice=' + configServer
-            + ' -Dexperiment.configuration.path=/FrinexBuildService/processing/staging-building'
-            //  2024-07-18 suppressing the inclusion of the desktop and mobile application artifacts to save server side disk space
-            + ' -Dexperiment.artifactsJsonDirectory=/FrinexBuildService/artifacts/${buildName}/included_artifacts/'
-            // + ' -Dexperiment.artifactsJsonDirectory=/FrinexBuildService/artifacts/${buildName}/'
-            + ' -DversionCheck.allowSnapshots=' + 'false'
-            // + ' -DversionCheck.buildType=' + 'stable'
-            + ' -Dexperiment.destinationServer=' + stagingServer
-            + ' -Dexperiment.destinationServerUrl=' + stagingServerUrl
-            + ' -Dexperiment.groupsSocketUrl=' + stagingGroupsSocketUrl
-            + ' -Dexperiment.configuration.db.host=' + stagingDbHost
-            + ' -Dexperiment.configuration.admin.allowDelete=' + ((currentEntry.allowDelete != null) ? currentEntry.allowDelete : 'false')
-            // admin login for staging is taken from the settings.xml rather than the publish.properties
-            // + ' -Dexperiment.configuration.admin.password=' + stagingAdminToken
-            + ' -Dexperiment.configuration.securityGroup=_security_group_'
-            + ' -Dexperiment.isScalable=' + currentEntry.isScalable
-            + ' -Dexperiment.defaultScale=' + currentEntry.defaultScale
-            + ' -Dexperiment.registrationUrl=' + currentEntry.registrationUrlStaging
-            + " &>> /FrinexBuildService/artifacts/${buildName}/${buildName}_staging_admin.txt;"
-            //+ ' rm -r /usr/local/tomcat/webapps/${buildName}_staging_admin.war'
-            // + " &>> /FrinexBuildService/artifacts/${buildName}/${buildName}_staging_admin.txt;"
-            //+ ' cp /ExperimentTemplate/registration/target/${buildName}-frinex-admin-*.war /usr/local/tomcat/webapps/${buildName}_staging_admin.war'
-            // + " &>> /FrinexBuildService/artifacts/${buildName}/${buildName}_staging_admin.txt;"
-            + ' cp /ExperimentTemplate/registration/target/${buildName}-frinex-admin-*-*.war ' + protectedDirectory + '/${buildName}/${buildName}_staging_admin.war'
-            + " &>> /FrinexBuildService/artifacts/${buildName}/${buildName}_staging_admin.txt;"
-            + ' mv /ExperimentTemplate/registration/target/${buildName}-frinex-admin-*-*-sources.jar /FrinexBuildService/artifacts/${buildName}/${buildName}_staging_admin_sources.jar'
-            + " &>> /FrinexBuildService/artifacts/${buildName}/${buildName}_staging_admin.txt;"
-            + " chmod 775 -R /FrinexBuildService/protected/${buildName}/;"
-            + " chmod 775 -R /FrinexBuildService/artifacts/${buildName}/;"
-            + " chown -R 101010 /FrinexBuildService/artifacts/${buildName}/;"
-            + " chown -R 101010 /FrinexBuildService/protected/${buildName}/;"
-            + ' echo "build complete" &>> /FrinexBuildService/artifacts/${buildName}/${buildName}_staging_admin.txt;'
-            + '"'
-            child_process.execSync(dockerString.replace("_security_group_", currentEntry.securityGroup ?? ''), { stdio: [0, 1, 2] });
+#   terminate existing docker containers by name 
+var buildContainerName = $buildName + '_staging_admin';
+        # // var dockerString = 'sudo docker container rm -f ' + buildContainerName
+        #     /* not currently required */ //+ ' --net="host" ' // enables the container to connect to ports on the host, so that maven can access tomcat manager
+        #     // # the maven settings and its .m2 directory need to be in the volume m2Directory:/maven/.m2/
+        #     // using sed to replace the destinationServerUrl with destinationServer for older build images, new build images did not need this but since the addition of the proxy it is now required for all
+        #     // using sed to replace the deprecated DB URL in very old build images like 1.3-audiofix
+        #     // + " sed -i 's|localhost:5432|" + stagingDbHost + "|g' /ExperimentTemplate/registration/src/main/resources/application.properties;"
+        #     // the target 'compile' is used to cause compilation errors to show up before all the effort/time of the full build process
+        #     /* currentEntry.isWebApp && isWebApp is incorrect, non web apps still need the admin */
+        #     /* limiting tomcat deployments to when a server is specified */ 
+        #     // quiet output
+        #     //  2024-07-18 suppressing the inclusion of the desktop and mobile application artifacts to save server side disk space
+        #     // admin login for staging is taken from the settings.xml rather than the publish.properties
+        #     // + ' -Dexperiment.configuration.admin.password=' + stagingAdminToken
+        #     //+ ' rm -r /usr/local/tomcat/webapps/' + currentEntry.buildName + '_staging_admin.war'
+        #     // + " &>> " + targetDirectory + "/" + currentEntry.buildName + "/" + currentEntry.buildName + "_staging_admin.txt;"
+        #     //+ ' cp /ExperimentTemplate/registration/target/' + currentEntry.buildName + '-frinex-admin-*.war /usr/local/tomcat/webapps/' + currentEntry.buildName + '_staging_admin.war'
+        #     // + " &>> " + targetDirectory + "/" + currentEntry.buildName + "/" + currentEntry.buildName + "_staging_admin.txt;"
+        # 2026-06-09 destinationServerUrl and destinationServer actions have been removed because they are tomcat only 
+            # + " sed -i 's|>\\${experiment.destinationServer}/manager/text|>https://\\${experiment.destinationServer}/manager/text|g' /ExperimentTemplate/pom.xml;"
+            # + " sed -i 's|>\\${experiment.destinationServerUrl}/manager/text|>https://\\${experiment.destinationServer}/manager/text|g' /ExperimentTemplate/pom.xml;"
+        # 2026-06-09 the following has also been removed :
+            # + ' rm /FrinexBuildService/processing/staging-building/' + currentEntry.buildName + '-frinex-gui-*-*-cordova.zip;'
+            # + ' rm /FrinexBuildService/processing/staging-building/' + currentEntry.buildName + '-frinex-gui-*-*-electron.zip;'
+            # + ' rm /FrinexBuildService/processing/staging-building/' + currentEntry.buildName + '-frinex-gui-*-*-vr.zip;'
+            # + ' mkdir ' + targetDirectory + '/' + currentEntry.buildName + '/included_artifacts/;'
+        # 2026-06-09 
+            # removed -Dexperiment.configuration.displayName=\\\"' + currentEntry.experimentDisplayName + '\\\"'
+        # 2026-06-10 
+            #             + ' -Dexperiment.isScalable=' + currentEntry.isScalable
+            # + ' -Dexperiment.defaultScale=' + currentEntry.defaultScale
+            # + ' -Dexperiment.registrationUrl=' + currentEntry.registrationUrlStaging
+            # -Dexperiment.groupsSocketUrl=$stagingGroupsSocketUrl \
+
+sudo docker container rm -f $buildContainerName &> /dev/null;
+sudo docker run --rm $buildContainerOptions \
+                --name $buildContainerName \
+                -v processingDirectory:/FrinexBuildService/processing \
+                -v buildServerTarget:/FrinexBuildService/artifacts \
+                -v protectedDirectory:/FrinexBuildService/protected \
+                -v m2Directory:/maven/.m2/ \
+                -w /ExperimentTemplate frinexapps-jdk:$frinexVersion \
+                /bin/bash -c "cd /ExperimentTemplate/registration; \
+                    mvn clean compile package \
+                    -gs /maven/.m2/settings.xml \
+                    -DskipTests \
+                    -q \
+                    -Dexperiment.configuration.name=$buildName \
+                    -Dexperiment.webservice=$configServer \
+                    -Dexperiment.configuration.path=/FrinexBuildService/processing/staging-building \
+                    -Dexperiment.artifactsJsonDirectory=/FrinexBuildService/artifacts/$buildName/included_artifacts/ \
+                    -DversionCheck.allowSnapshots=false \
+                    -Dexperiment.destinationServer=$stagingServer \
+                    -Dexperiment.destinationServerUrl=$stagingServerUrl \
+                    -Dexperiment.configuration.db.host=$stagingDbHost \
+                    -Dexperiment.configuration.admin.allowDelete=$allowDelete \
+                    -Dexperiment.configuration.securityGroup=$securityGroup \
+                    &>> /FrinexBuildService/artifacts/$buildName/${buildName}_staging_admin.txt; \
+                    cp /ExperimentTemplate/registration/target/${buildName}-frinex-admin-*-*.war /FrinexBuildService/protected/$buildName/${buildName}_staging_admin.war' \
+                    &>> /FrinexBuildService/artifacts/$buildName/${buildName}_staging_admin.txt; \
+                    mv /ExperimentTemplate/registration/target/${buildName}-frinex-admin-*-*-sources.jar /FrinexBuildService/artifacts/$buildName/${buildName}_staging_admin_sources.jar \
+                    &>> /FrinexBuildService/artifacts/$buildName/${buildName}_staging_admin.txt; \
+                    chmod 775 -R /FrinexBuildService/protected/$buildName/; \
+                    chmod 775 -R /FrinexBuildService/artifacts/$buildName/; \
+                    chown -R 101010 /FrinexBuildService/artifacts/$buildName/; \
+                    chown -R 101010 /FrinexBuildService/protected/$buildName/; \
+                    echo \"build complete\" &>> /FrinexBuildService/artifacts/$buildName/${buildName}_staging_admin.txt; \
+                ";
+            
